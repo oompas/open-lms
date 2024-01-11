f081d2c3fbc202857cd76af9477a27c87a34a7d0
Reid Moffat <18rem8@queensu.ca>
2024-01-11 17:48:49 -0500

    export

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,13 +7,8 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+import { createAccount, resetPassword, beforeCreate, onUserSignup, beforeSignIn, onUserDelete } from "./auth"
+import { purgeUnverifiedUsers, purgeExpiredEmails } from "./cron"
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export { createAccount, resetPassword, beforeCreate, onUserSignup, beforeSignIn, onUserDelete, purgeUnverifiedUsers, purgeExpiredEmails }
