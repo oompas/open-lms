f834bb32d6ab07f1c78c6a1e57380e60086b4092
Reid Moffat <18rem8@queensu.ca>
2024-01-19 12:00:24 -0500

    test index file

---
+++ functions/test/index.test.ts
@@ -0,0 +1,35 @@
+import xyx from "./serviceAccountKey.json" with { type: "json" };
+import functionsTest from "firebase-functions-test";
+
+//
+// !!! Do not change this file unless you know EXACTLY what you're doing !!!
+// The order of code here matters significantly, and confidential info is used that can't be exposed
+// See https://firebase.google.com/docs/functions/unit-testing for setup info
+//
+
+console.log("Initializing function tests data...");
+const test = functionsTest({
+    storageBucket: xyx.project_id + '.appspot.com',
+    projectId: xyx.project_id,
+}, './test/serviceAccountKey.json');
+console.log("Done");
+
+// If you need to mock config values with functions.config(), put them here
+
+console.log("Getting all firebase functions...");
+// Do not move to the top; this must be done AFTER initializing firebase-functions-test and mocking config values
+import * as functions from "../src";
+console.log("Done");
+
+it('Should complete', (done) => {
+
+    const data2 = test.firestore.makeDocumentSnapshot({ email: "testEmail@gmail.com", password: "pass98765" }, 'document/path');
+
+    const wrapped = test.wrap(functions.createAccount);
+    wrapped(data2);
+
+    done();
+})
+
+// Unsets envars & deletes temporary objects
+test.cleanup();
