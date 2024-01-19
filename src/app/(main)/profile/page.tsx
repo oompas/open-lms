68449d5fd02d1969bda0d208c64827f71a0d7995
Sara Hall <sara@lifenotes.ca>
2024-01-19 00:15:01 -0500

    Initial commit

--- src/app/(main)/profile/page.tsx
+++ src/app/(main)/profile/page.tsx
@@ -1,7 +1,36 @@
+import CompletedCourse from "./CompletedCourse";
+import Button from "@/components/Button";
+
 export default function Profile() {
+
+    const TEMP_COMPLETED_COURSE_DATA = [
+        { title: "Completed Course", description: "Completed December 25, 2023", id: 1 },
+        { title: "Completed Course", description: "Completed January 5, 2024", id: 2 }
+    ]
+
     return (
-        <main className="flex items-center justify-center">
-            PROFILE PAGE
+        <main className="flex justify-center pt-14">
+            <div className="flex flex-col h-[80vh] bg-white w-[60%] p-16 rounded-2xl shadow-custom">
+                <div className="text-2xl mb-8">Account Details</div>
+                <div className="flex flex-row flex-wrap justify-between overflow-y-scroll sm:no-scrollbar">
+                    <Button text={"Delete Account"} link="/home"/>
+                </div>
+            </div>
+            <div className="flex flex-col h-[80vh] bg-white w-[35%] ml-[5%] p-16 rounded-2xl shadow-custom">
+                <div className="flex flex-row mb-8">
+                    <div className="text-2xl mr-auto">Completed Courses</div>
+                </div>
+                <div className="flex flex-col justify-between overflow-y-scroll sm:no-scrollbar">
+                    {TEMP_COMPLETED_COURSE_DATA.map((course, key) => (
+                        <CompletedCourse
+                            key={key}
+                            title={course.title}
+                            date={course.description}
+                            id={course.id}
+                        />
+                    ))}
+                </div>
+            </div>
         </main>
     )
 }
