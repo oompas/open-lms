41feed27938aa4e9dcbb8638d915fce062a8b1f6
Reid Moffat <18rem8@queensu.ca>
2024-01-21 04:30:08 -0500

    dummy files

--- functions/test/helpers/runOrder.ts
+++ functions/test/helpers/runOrder.ts
@@ -1,4 +1,30 @@
 import "../callable/auth/createAccount";
 import "../callable/auth/resetPassword";
+import "../callable/auth/getUserProfile";
 
+import "../callable/courses/courseEnroll";
+import "../callable/courses/getAvailableCourses";
+import "../callable/courses/getCourseInfo";
+import "../callable/courses/saveCourse";
+import "../callable/courses/sendCourseFeedback";
+import "../callable/courses/startCourse";
+
+import "../callable/misc/sendPlatformFeedback";
+
+import "../callable/quizzes/getQuizResponses";
+import "../callable/quizzes/saveQuiz";
+import "../callable/quizzes/startQuiz";
+import "../callable/quizzes/submitQuiz";
+
+import "../callable/reports/getCourseReports";
+import "../callable/reports/getUserReports";
+
+import "../triggers/auth/beforeSignIn";
+import "../triggers/auth/onUserDelete";
+import "../triggers/auth/onUserSignup";
+
+import "../triggers/database/changeUserPosition";
+
+//
 // Add an import for all test files (except setup/cleanup) here in the desired order
+//
