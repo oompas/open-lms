a52eb91856651567eee69f5100a6bd71ee3386bd
Reid Moffat <18rem8@queensu.ca>
2024-01-21 03:50:17 -0500

    reset password tests

--- functions/test/callable/auth/createAccount.ts
+++ functions/test/callable/auth/createAccount.ts
@@ -8,8 +8,8 @@ import { dummyAccount } from "../../helpers/setupDummyData";
 
 interface TestInput {
     description: string,
-    testEmail: any,
-    testPassword: any,
+    email: any,
+    password: any,
 }
 let testNumber = 0; // Increment each time
 
@@ -22,8 +22,8 @@ const testCreateAccount = (input: TestInput, errorMessage: undefined | string) =
             it(message, () => {
 
                 const data = testEnv.firestore.makeDocumentSnapshot({
-                    email: input.testEmail,
-                    password: input.testPassword
+                    email: input.email,
+                    password: input.password
                 }, `TestInput/createAccount#${testNumber}`);
 
                 // Check if the call passes or fails as desired
@@ -40,7 +40,7 @@ const testCreateAccount = (input: TestInput, errorMessage: undefined | string) =
 
                 // @ts-ignore
                 return testEnv.wrap(createAccount)(data).then(async (result: string) => {
-                    assert.equal(result, `Successfully created new user ${input.testEmail}`,
+                    assert.equal(result, `Successfully created new user ${input.email}`,
                         "Response message does not match expected");
                 });
             })
@@ -54,7 +54,7 @@ describe('Success cases for createAccount endpoint...', () => {
     const testAccounts: string[] = [];
     const test = () => {
         testCreateAccount(testData, undefined);
-        testAccounts.push(testData.testEmail);
+        testAccounts.push(testData.email);
     }
 
     // Clean up accounts after tests are done
@@ -66,56 +66,56 @@ describe('Success cases for createAccount endpoint...', () => {
         await Promise.all(testAccounts.map((email) => {
             return auth.getUserByEmail(email)
                 .then((user: { uid: any; }) => auth.deleteUser(user.uid))
-                .catch((err: any) => { throw new HttpsError('internal', `Error getting uid for ${testData.testEmail}: ${err}`); });
+                .catch((err: any) => { throw new HttpsError('internal', `Error getting uid for ${testData.email}: ${err}`); });
         }));
     });
 
     testData = {
         description: "Gmail #1",
-        testEmail: "firebase_unit_tests_create_account_1@gmail.com",
-        testPassword: "password12345",
+        email: "firebase_unit_tests_create_account_1@gmail.com",
+        password: "password12345",
     };
     test();
 
     testData = {
         description: "Gmail #2",
-        testEmail: "firebase_unit_tests_create_account_2@gmail.com",
-        testPassword: "password12345",
+        email: "firebase_unit_tests_create_account_2@gmail.com",
+        password: "password12345",
     };
     test();
 
     testData = {
         description: "queensu",
-        testEmail: "firebase_unit_tests_create_account_3@queensu.ca",
-        testPassword: "password12345",
+        email: "firebase_unit_tests_create_account_3@queensu.ca",
+        password: "password12345",
     };
     test();
 
     testData = {
         description: "outlook",
-        testEmail: "firebase_unit_tests_create_account_4@outlook.com",
-        testPassword: "password12345",
+        email: "firebase_unit_tests_create_account_4@outlook.com",
+        password: "password12345",
     };
     test();
 
     testData = {
         description: "yahoo",
-        testEmail: "firebase_unit_tests_create_account_5@yahoo.com",
-        testPassword: "password12345",
+        email: "firebase_unit_tests_create_account_5@yahoo.com",
+        password: "password12345",
     };
     test();
 
     testData = {
         description: `Minimum password length `,
-        testEmail: `firebase_unit_tests_create_account_${6}@gmail.com`,
-        testPassword: randomString(6),
+        email: `firebase_unit_tests_create_account_${6}@gmail.com`,
+        password: randomString(6),
     };
     test();
 
     testData = {
         description: `Maximum password length`,
-        testEmail: `firebase_unit_tests_create_account_${7}@gmail.com`,
-        testPassword: randomString(100),
+        email: `firebase_unit_tests_create_account_${7}@gmail.com`,
+        password: randomString(100),
     };
     test();
 
@@ -124,8 +124,8 @@ describe('Success cases for createAccount endpoint...', () => {
         const length = Math.floor(Math.random() * 79) + 21;
         testData = {
             description: `Password length ${length}`,
-            testEmail: `firebase_unit_tests_create_account_${8 + i}@gmail.com`,
-            testPassword: randomString(length),
+            email: `firebase_unit_tests_create_account_${8 + i}@gmail.com`,
+            password: randomString(length),
         };
         test();
     }
@@ -133,108 +133,107 @@ describe('Success cases for createAccount endpoint...', () => {
 
 describe('Failure cases for createAccount endpoint...', () => {
 
+    testNumber = 0;
     let testData: TestInput;
-    const test = (errMsg: string) => {
-        testCreateAccount(testData, errMsg);
-    }
+    const test = (errMsg: string) => testCreateAccount(testData, errMsg);
 
     testData = {
         description: `Invalid email #1`,
-        testEmail: `test.at.test.com`,
-        testPassword: "password12345",
+        email: `test.at.test.com`,
+        password: "password12345",
     };
-    test(`Email ${testData.testEmail} is invalid`);
+    test(`Email ${testData.email} is invalid`);
 
     testData = {
         description: `Invalid email #2`,
-        testEmail: `test@test`,
-        testPassword: "password12345",
+        email: `test@test`,
+        password: "password12345",
     };
-    test(`Email ${testData.testEmail} is invalid`);
+    test(`Email ${testData.email} is invalid`);
 
     testData = {
         description: `Invalid email #3`,
-        testEmail: `open LMS@gmail.com`,
-        testPassword: "password12345",
+        email: `open LMS@gmail.com`,
+        password: "password12345",
     };
-    test(`Email ${testData.testEmail} is invalid`);
+    test(`Email ${testData.email} is invalid`);
 
     testData = {
         description: `Invalid email #4`,
-        testEmail: `test@test@com`,
-        testPassword: "password12345",
+        email: `test@test@com`,
+        password: "password12345",
     };
-    test(`Email ${testData.testEmail} is invalid`);
+    test(`Email ${testData.email} is invalid`);
 
     testData = {
         description: `Invalid email #5`,
-        testEmail: "",
-        testPassword: "password12345",
+        email: "",
+        password: "password12345",
     };
     test(`The parameter email is required`);
 
     testData = {
         description: `Invalid email #6`,
-        testEmail: null,
-        testPassword: "password12345",
+        email: null,
+        password: "password12345",
     };
     test(`The parameter email is required`);
 
     testData = {
         description: `Invalid email #7`,
-        testEmail: 12345,
-        testPassword: "password12345",
+        email: 12345,
+        password: "password12345",
     };
     test(`The parameter email is required`);
 
     testData = {
         description: `Email in use`,
-        testEmail: dummyAccount.email,
-        testPassword: "password123456",
+        email: dummyAccount.email,
+        password: "password123456",
     };
-    test(`Email ${testData.testEmail} is already in use`);
+    test(`Email ${testData.email} is already in use`);
 
     testData = {
         description: `Invalid password #1`,
-        testEmail: `test@test.com`,
-        testPassword: ""
+        email: `test@test.com`,
+        password: ""
     };
     test(`The parameter password is required`);
 
     testData = {
         description: `Invalid password #2`,
-        testEmail: `test@test.com`,
-        testPassword: null
+        email: `test@test.com`,
+        password: null
     };
     test(`The parameter password is required`);
 
     testData = {
         description: `Invalid password #3`,
-        testEmail: `test@test.com`,
-        testPassword: 12345
+        email: `test@test.com`,
+        password: 12345
     };
     test(`The parameter password is required`);
 
     for (let i = 1; i < 6; ++i) {
         testData = {
             description: `Invalid password #${i + 3}`,
-            testEmail: `test@test.com`,
-            testPassword: randomString(i)
+            email: `test@test.com`,
+            password: randomString(i)
         };
         test(`Password is invalid. It must be a string with at least six characters.`);
     }
 
     testData = {
         description: `Invalid password #10`,
-        testEmail: `test@test.com`,
-        testPassword: randomString(101)
+        email: `test@test.com`,
+        password: randomString(101)
     };
     test(`Password can't be over 100 characters long`);
 
     testData = {
         description: `Invalid password #11`,
-        testEmail: `test@test.com`,
-        testPassword: randomString(1000)
+        email: `test@test.com`,
+        password: randomString(1000)
     };
     test(`Password can't be over 100 characters long`);
 });
--- functions/test/helpers/runOrder.ts
+++ functions/test/helpers/runOrder.ts
@@ -1,3 +1,4 @@
 import "../callable/auth/createAccount";
+import "../callable/auth/resetPassword";
 
 // Add an import for all test files (except setup/cleanup) here in the desired order
