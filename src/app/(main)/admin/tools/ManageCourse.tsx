2e742dd588771f15ad89346eae6b702fc9967058
Sara Hall <sara@lifenotes.ca>
2024-01-23 01:12:17 -0500

    Starting point

---
+++ src/app/(main)/admin/tools/ManageCourse.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link"
+
+export default function ManageCourse({
+    title,
+    description,
+    id
+} : {
+    title: string,
+    description: string,
+    id: number
+}) {
+    return (
+        <Link
+            className="flex-shrink-0 w-[379px] mr-4 border-4 border-gray-300 mb-4 p-4 rounded-2xl cursor-pointer hover:opacity-60 duration-100"
+            href={"/course/"+id}
+        >
+            <div className="text-2xl">{title}</div>
+            <div className="mt-1 text-lg">{description}</div>
+        </Link>
+    )
+}
--- src/app/(main)/admin/tools/page.tsx
+++ src/app/(main)/admin/tools/page.tsx
@@ -1,7 +1,52 @@
+import ManageCourse from "@/app/(main)/admin/tools/ManageCourse";
+import TextField from "@/components/TextField";
+import Button from "@/components/Button";
+import SearchBar from "@/components/SearchBar";
+
+const TEMP_MANAGE_COURSE_DATA = [
+    { title: "Available Course on OpenLMS Platform", description: "Example course description briefly describing the course contents.", id: 10 },
+    { title: "Available Course on OpenLMS Platform", description: "Example course description briefly describing the course contents.", id: 11 },
+    { title: "Available Course on OpenLMS Platform", description: "Example course description briefly describing the course contents.", id: 12 },
+    { title: "Available Course on OpenLMS Platform", description: "Example course description briefly describing the course contents.", id: 13 },
+    { title: "Available Course on OpenLMS Platform", description: "Example course description briefly describing the course contents.", id: 14 },
+    { title: "Available Course on OpenLMS Platform", description: "Example course description briefly describing the course contents.", id: 15 },
+    { title: "Available Course on OpenLMS Platform", description: "Example course description briefly describing the course contents.", id: 16 },
+    { title: "Available Course on OpenLMS Platform", description: "Example course description briefly describing the course contents.", id: 17 },
+    { title: "Available Course on OpenLMS Platform", description: "Example course description briefly describing the course contents.", id: 18 },
+    { title: "Available Course on OpenLMS Platform", description: "Example course description briefly describing the course contents.", id: 19 }
+]
+
 export default function Tools() {
     return (
-        <main className="flex items-center justify-center">
-            PLATFORM TOOLS PAGE
+        <main className="flex-col justify-center items-center pt-14">
+            <div className="flex flex-col h-[50vh] bg-white p-16 rounded-2xl shadow-custom mb-8">
+                <div className="text-2xl mb-2">Manage Courses</div>
+                <div className="flex flex-row justify-between items-start mb-2">
+                    <p className="mb-0 mr-2">Click on a course to navigate to course update screen.</p>
+                    <div className="flex flex-row">
+                        <Button text="Create a Course" link="/home"/>
+                        <SearchBar/>
+                    </div>
+                </div>
+                <div className="flex flex-wrap justify-start overflow-y-scroll sm:no-scrollbar">
+                    {TEMP_MANAGE_COURSE_DATA.map((course, key) => (
+                        <ManageCourse
+                            key={key}
+                            title={course.title}
+                            description={course.description}
+                            id={course.id}
+                        />
+                    ))}
+                </div>
+            </div>
+            <div className="flex flex-col h-[50vh] bg-white p-16 rounded-2xl shadow-custom mb-8">
+                <div className="text-2xl mb-8">Learner Insights</div>
+                {/* Add your content for the second section here */}
+            </div>
+            <div className="flex flex-col h-[50vh] bg-white p-16 rounded-2xl shadow-custom">
+                <div className="text-2xl mb-8">Course Insights</div>
+                {/* Add your content for the second section here */}
+            </div>
         </main>
     )
 }
