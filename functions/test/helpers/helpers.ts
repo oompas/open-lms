d3026034d9dbb17cff3f0fe20d2c079c98b94b01
Reid Moffat <18rem8@queensu.ca>
2024-01-27 17:25:39 -0500

    call with auth helper

--- functions/test/helpers/helpers.ts
+++ functions/test/helpers/helpers.ts
@@ -1,3 +1,7 @@
+import { signInWithEmailAndPassword, signOut } from "@firebase/auth";
+import { auth, functions } from "./clientSetup";
+import { httpsCallable } from "firebase/functions";
+
 // Generates a string of the specified length of random characters
 const randomString = (length: number): string => {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~`!@#$%^&*()_+-={}[]|\\:";\'<>,.?/';
@@ -12,7 +16,22 @@ const randomString = (length: number): string => {
     return result;
 }
 
+// Calls an onCall function for a given user
+const callFunctionWithAuth = async (functionName: string, data: any, email: string, password: string) => {
+    if (auth.currentUser) {
+        await signOut(auth)
+            .then(() => console.log(`Successfully signed out of account ${email}`))
+            .catch((err) => { throw new Error(`Error signing out: ${err}`); });
+    }
+
+    await signInWithEmailAndPassword(auth, email, password)
+        .then(() => console.log(`Successfully logged in to account ${email}`))
+        .catch((err) => { throw new Error(`Error signing into ${email}: ${err}`); });
+
+    return httpsCallable(functions, functionName)(data);
+}
+
 const USER_ID_LENGTH: number = 28;
 const DOCUMENT_ID_LENGTH: number = 20;
 
-export { randomString, USER_ID_LENGTH, DOCUMENT_ID_LENGTH };
+export { randomString, callFunctionWithAuth, USER_ID_LENGTH, DOCUMENT_ID_LENGTH };
