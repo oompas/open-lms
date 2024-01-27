eb67683514a5269ca7c30069c131468b90bb7aca
Reid Moffat <18rem8@queensu.ca>
2024-01-27 17:02:53 -0500

    client setup on test

---
+++ functions/test/helpers/clientSetup.ts
@@ -0,0 +1,12 @@
+import * as firebaseConfig from "../config.json" with { type: "json" };
+import { initializeApp } from "firebase/app";
+import { getAuth } from "@firebase/auth";
+import { getFunctions } from "firebase/functions";
+
+// There doesn't seem to be a way to call functions with firebase-functions-test and authentication
+// So to call functions that require user auth, just do it like a client app
+const clientApp = initializeApp(firebaseConfig);
+const auth = getAuth(clientApp);
+const functions = getFunctions(clientApp);
+
+export { clientApp, auth, functions };
