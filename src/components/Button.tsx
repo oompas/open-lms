6f1c80b024a076fa5870ec5743bca138950ec5b4
Graham Carkner <gcarkner@gmail.com>
2024-01-27 12:47:39 -0500

    style adjustments

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,7 +10,7 @@ export default function Button({
     return (
         <Link
             href={link}
-            className="p-3 w-fit border-[3px] border-red-800 rounded-lg duration-75 ease-out hover:px-4 cursor-pointer"
+            className="flex p-3 w-fit border-[3px] border-red-800 rounded-lg duration-75 ease-out hover:px-4 cursor-pointer"
         >
             <div>{text}</div>
         </Link>
