ccc72ddf3717aabb4514684782f82f0c7c7bc6f9
Reid Moffat <18rem8@queensu.ca>
2024-01-28 13:34:10 -0500

    clean extra private in package.json

--- functions/test/run.sh
+++ functions/test/run.sh
@@ -3,6 +3,9 @@
 # Need this to be a module for tests
 sed -i '$ d' package.json
 sed -i '$ d' package.json
+if [[ $(tail -n 1 package.json) == "  \"private\": true," ]]; then
+  sed -i '$ d' package.json # If you exit the script partway through, an extra line is added
+fi
 echo "  \"private\": true," >> package.json
 echo "  \"type\": \"module\"" >> package.json
 echo "}" >> package.json
