6cf101427abccee2e1d24e456a4d5f792bcc0ca4
Reid Moffat <18rem8@queensu.ca>
2024-01-28 13:47:47 -0500

    simplify run script

--- functions/test/run.sh
+++ functions/test/run.sh
@@ -2,13 +2,7 @@
 
 # Need this to be a module for tests
 sed -i '$ d' package.json
-sed -i '$ d' package.json
-if [[ $(tail -n 1 package.json) == "  \"private\": true," ]]; then
-  sed -i '$ d' package.json # If you exit the script partway through, an extra line is added
-fi
-echo "  \"private\": true," >> package.json
-echo "  \"type\": \"module\"" >> package.json
-echo "}" >> package.json
+printf "  ,\"type\": \"module\"\n}" >> package.json
 
 # Set envar for service account location (so tests can access services)
 export GOOGLE_APPLICATION_CREDENTIALS=test/serviceAccountKey.json
@@ -17,6 +11,4 @@ mocha --reporter spec
 # Remove module type
 sed -i '$ d' package.json
 sed -i '$ d' package.json
-sed -i '$ d' package.json
-echo "  \"private\": true" >> package.json
 echo "}" >> package.json
