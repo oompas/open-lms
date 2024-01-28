239c27e47ff12859d0e86f020699a2c965b4c651
Reid Moffat <18rem8@queensu.ca>
2024-01-28 16:46:14 -0500

    add params to test run script

--- functions/test/run.sh
+++ functions/test/run.sh
@@ -6,7 +6,19 @@ printf "  ,\"type\": \"module\"\n}" >> package.json
 
 # Set envar for service account location (so tests can access services)
 export GOOGLE_APPLICATION_CREDENTIALS=test/serviceAccountKey.json
-mocha --reporter spec
+
+if [ $# -eq 0 ]; then
+  echo "===================="
+  echo "Running all tests..."
+  echo "===================="
+  mocha --reporter spec
+else
+  echo "============================================="
+  echo "Running tests with $1 in their name..."
+  echo "============================================="
+  mocha --reporter spec -g "$1"
+fi
+
 
 # Remove module type
 sed -i '$ d' package.json
