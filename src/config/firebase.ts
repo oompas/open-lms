0d9410fcc6c570813dc124982535bbc1921dbbc0
Reid Moffat <18rem8@queensu.ca>
2024-01-29 12:28:15 -0500

    client auth

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "@firebase/auth";
 
 // Firebase configuration
 // To switch between dev & prod, swap your .env.local file
@@ -13,3 +14,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export { app, auth };
