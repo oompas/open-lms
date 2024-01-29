2a6c0b55818ce58de7e1c1d3d8148c68cb7cd373
Reid Moffat <18rem8@queensu.ca>
2024-01-29 12:20:55 -0500

    Revert "one file for fb config"
    This reverts commit 797cdf2a5e09cc93f11450e449bb8af488e2ce92.

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,10 +1,15 @@
-// @ts-ignore
-import * as config from "../../firebaseConfig.json" with { type: "json" };
 import { initializeApp } from "firebase/app";
-import { getAuth } from "@firebase/auth";
 
-const app = initializeApp(config);
+// Firebase configuration
+// To switch between dev & prod, swap your .env.local file
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+};
 
-const auth = getAuth(app);
-
-export { app, auth };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
