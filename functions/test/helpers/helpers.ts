7e71416a94177f52204174f8f54f7bc1619b0032
Reid Moffat <18rem8@queensu.ca>
2024-02-02 16:34:34 -0500

    helper re-naming

--- functions/test/helpers/helpers.ts
+++ functions/test/helpers/helpers.ts
@@ -1,5 +1,5 @@
 import { signInWithEmailAndPassword, signOut } from "@firebase/auth";
-import { auth, functions } from "./clientSetup";
+import { clientAuth, clientFunctions } from "./clientSetup";
 import { httpsCallable } from "firebase/functions";
 
 // Generates a string of the specified length of random characters
@@ -17,21 +17,21 @@ const randomString = (length: number): string => {
 }
 
 // Calls an onCall function unauthenticated
-const callOnCallFunction = async (functionName: string, data: any) => httpsCallable(functions, functionName)(data);
+const callOnCallFunction = async (functionName: string, data: any) => httpsCallable(clientFunctions, functionName)(data);
 
 // Calls an onCall function for a given user
 const callOnCallFunctionWithAuth = async (functionName: string, data: any, email: string, password: string) => {
-    if (auth.currentUser) {
-        await signOut(auth)
+    if (clientAuth.currentUser) {
+        await signOut(clientAuth)
             .then(() => console.log(`Successfully signed out of account ${email}`))
             .catch((err) => { throw new Error(`Error signing out: ${err}`); });
     }
 
-    await signInWithEmailAndPassword(auth, email, password)
+    await signInWithEmailAndPassword(clientAuth, email, password)
         .then(() => console.log(`Successfully logged in to account ${email}`))
         .catch((err) => { throw new Error(`Error signing into ${email}: ${err}`); });
 
-    return httpsCallable(functions, functionName)(data);
+    return httpsCallable(clientFunctions, functionName)(data);
 }
 
 const USER_ID_LENGTH: number = 28;
