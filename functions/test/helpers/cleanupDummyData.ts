24c58d5b090b4f7a3b2e1f9a4df45aa06aac4d85
Reid Moffat <18rem8@queensu.ca>
2024-02-02 16:33:10 -0500

    better cleanup function

--- functions/test/helpers/cleanupDummyData.ts
+++ functions/test/helpers/cleanupDummyData.ts
@@ -1,21 +1,35 @@
 import { HttpsError } from "firebase-functions/v2/https";
-import { getAuth } from "firebase-admin/auth";
-import { dummyAccount } from "./setupDummyData";
 import "./runOrder";
 import testEnv from "../index.test";
+import { adminAuth, adminDb } from "./adminSetup";
 
 describe("Clean up dummy account", () => {
     after(() => {
         testEnv.cleanup;
     });
 
-    it("Delete dummy account", async function() {
-        this.timeout(30_000);
-        await new Promise(res => setTimeout(res, 10_000)); // Make sure all triggers are done
+    it("Delay cleanup so all triggers can finish...", async function() {
+        this.timeout(21_000);
+        await new Promise(res => setTimeout(res, 20_000)); // Make sure the onUserSignup() triggers are all done
+    });
 
-        const auth = getAuth();
-        return auth.getUserByEmail(dummyAccount.email)
-            .then((user: { uid: any; }) => auth.deleteUser(user.uid))
-            .catch((err: any) => { throw new HttpsError('internal', `Error getting uid for ${dummyAccount.email}: ${err}`); });
+    it("Delete test user accounts", async function() {
+        this.timeout(30_000);
+
+        // Accounts
+        const testUsers: { id: string, email: string }[] = await adminDb.collection("/User/")
+            .where("unitTest", "==", true)
+            .get()
+            .then((users) => users.docs.map((doc) => ({ id: doc.id, email: doc.data().email })))
+            .catch((err) => { throw new HttpsError('internal', `Error getting test users: ${err}`); });
+        const numTestUsers = testUsers.length;
+
+        await Promise.all(testUsers.map((user) =>
+            adminAuth.deleteUser(user.id)
+                .then(() => console.log(`Successfully deleted test user ${user.email} (${user.id})`))
+                .catch((err) => { throw new HttpsError('internal', `Error deleting user ${user.email} (${user.id}): ${err}`); })
+        ))
+            .then(() => console.log(`Successfully deleted ${numTestUsers} test users`))
+            .catch(() => { throw new HttpsError('internal', `Error deleting test users`); });
     });
 });
