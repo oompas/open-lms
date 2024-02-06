749519c8f2c0440c40d9961a1824f31b954a9ce5
Reid Moffat <18rem8@queensu.ca>
2024-02-06 11:42:41 -0500

    setup cleaning data

--- functions/test/helpers/setupDummyData.ts
+++ functions/test/helpers/setupDummyData.ts
@@ -1,36 +1,48 @@
 import { expect } from "chai";
 import { callOnCallFunction, randomString, TEST_EMAIL_PREFIX } from "./helpers";
 import { adminAuth, adminDb } from "./adminSetup";
+import { addTestUser } from "./testDataToClean";
 
 const dummyLearnerAccount = { email: TEST_EMAIL_PREFIX + "dummy_learner_account@gmail.com", password: randomString(20) };
 
 describe("Create dummy learner account", () => {
     it("Create dummy account", () => {
-        return callOnCallFunction("createAccount", dummyLearnerAccount).then((result) =>
-            expect(result).to.equal(`Successfully created new user ${dummyLearnerAccount.email}`)
-        )
+        return callOnCallFunction("createAccount", dummyLearnerAccount).then((result) => {
+            expect(result.data).to.be.a('string');
+            expect(result.data).to.match(new RegExp("^[a-zA-Z0-9]{28}$"));
+            addTestUser(dummyLearnerAccount.email, <string> result.data);
+            return <string>result.data;
+        })
     });
 });
 
 const dummyAdminAccount = { email: TEST_EMAIL_PREFIX + "dummy_admin_account@gmail.com", password: randomString(20) };
 
 describe("Create dummy admin account", () => {
-    it("Create dummy account", async () => {
+    it("Create dummy account", async function() {
+        this.timeout(40_000);
+
         console.log(`Creating dummy admin account ${dummyAdminAccount.email}`);
-        await callOnCallFunction("createAccount", dummyAdminAccount).then((result) => {
-            expect(result).to.equal(`Successfully created new user ${dummyAdminAccount.email}`);
+        const uid: string = await callOnCallFunction("createAccount", dummyAdminAccount).then((result) => {
+            expect(result.data).to.be.a('string');
+            expect(result.data).to.match(new RegExp("^[a-zA-Z0-9]{28}$"));
+            addTestUser(dummyAdminAccount.email, <string> result.data);
+            return <string> result.data;
         });
 
-        console.log(`Getting uid for ${dummyAdminAccount.email}`);
-        const uid = await adminAuth.getUserByEmail(dummyAdminAccount.email)
-            .then((user) => user.uid)
-            .catch((err) => { throw new Error(`Error getting admin user's uid: ${err}`); });
+        // console.log(`Getting uid for ${dummyAdminAccount.email}`);
+        // const uid = await adminAuth.getUserByEmail(dummyAdminAccount.email)
+        //     .then((user) => user.uid)
+        //     .catch((err) => { throw new Error(`Error getting admin user's uid: ${err}`); });
+
+        await new Promise(res => setTimeout(res, 10_000)); // Make sure the User document was created
 
         console.log(`Updating user document for ${dummyAdminAccount.email} to admin permissions`);
         await adminDb.doc(`/User/${uid}`)
             .update({ admin: true })
             .then(() => console.log(`Successfully updated user document for ${dummyAdminAccount.email} to admin permissions`))
             .catch((err) => { throw new Error(`Error updating user document for ${dummyAdminAccount.email} to admin permissions: ${err}`); });
+        await new Promise(res => setTimeout(res, 10_000)); // Wait for the custom claims to be updated
 
         console.log(`Verifying user's custom claims include admin permissions`);
         return adminAuth.getUser(uid)
--- functions/test/helpers/testDataToClean.ts
+++ functions/test/helpers/testDataToClean.ts
@@ -1,15 +1,20 @@
-import { existsSync, readFileSync, writeFileSync } from "fs";
+import { existsSync, readFileSync, writeFileSync, mkdirSync } from "fs";
 
-const testUserFilePath = "./test/testUsers.json";
+const tmpDirPath = "./test/tmp";
+const testUserFilePath = tmpDirPath + `/testUsers-${new Date().getTime()}.json`;
 
 const addTestUser = (email: string, uid: string) => {
+    if (!existsSync(tmpDirPath)) {
+        mkdirSync(tmpDirPath, { recursive: true });
+    }
+
     const usersJson: { email: string, uid: string }[] = existsSync(testUserFilePath)
         ? JSON.parse(readFileSync(testUserFilePath, "utf-8"))
         : [];
 
     usersJson.push({email, uid});
 
-    writeFileSync(testUserFilePath, JSON.stringify(usersJson), "utf-8");
+    writeFileSync(testUserFilePath, JSON.stringify(usersJson, null, 4), "utf-8");
 }
 
-export { addTestUser, testUserFilePath };
+export { addTestUser, testUserFilePath, tmpDirPath };
