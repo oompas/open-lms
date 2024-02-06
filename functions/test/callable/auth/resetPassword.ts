28829d352da835247b30c829e0ea7c31e9d2f4c2
Reid Moffat <18rem8@queensu.ca>
2024-02-06 11:41:32 -0500

    update pass reset tests

--- functions/test/callable/auth/resetPassword.ts
+++ functions/test/callable/auth/resetPassword.ts
@@ -1,53 +1,51 @@
 import { dummyLearnerAccount, dummyAdminAccount } from "../../helpers/setupDummyData";
 import testEnv from "../../index.test";
 import { resetPassword } from "../../../src";
-import { assert } from "chai";
-
-interface TestInput {
-    description: string,
-    email: any,
-}
-let testNumber = 0; // Increment each time
-
-const testResetPassword = (input: TestInput, errorMessage: undefined | string) => {
-    ++testNumber;
-    const message = errorMessage ? "fail to reset password" : "reset password successfully";
-
-    return (
-        describe(`#${testNumber}: ` + input.description, () => {
-            it(message, () => {
-
-                const data = testEnv.firestore.makeDocumentSnapshot({
-                    email: input.email,
-                }, `TestInput/resetPassword#${testNumber}`);
-
-                // Check if the call passes or fails as desired
-                if (errorMessage) {
-                    try { // @ts-ignore
-                        return testEnv.wrap(resetPassword)(data)
-                            .then(() => { throw new Error("API call should fail"); })
-                            .catch((err: any) => assert.equal(err.message, errorMessage));
-                    } catch (err) { // @ts-ignore
-                        assert.equal(err.message, errorMessage);
-                        return;
-                    }
-                }
-
-                // @ts-ignore
-                return testEnv.wrap(resetPassword)(data).then(async (result: string) => {
-                    assert.equal(result, `Password reset email created for ${input.email}`,
-                        "Response message does not match expected");
-                });
-            })
-        })
-    );
-}
+import { assert, expect } from "chai";
+import { callOnCallFunction } from "../../helpers/helpers";
 
 describe('Success cases for resetPassword endpoint...', () => {
 
-    let testData: TestInput;
+    interface TestInput {
+        description: string,
+        email: any,
+    }
 
-    const test = () => testResetPassword(testData, undefined);
+    let testNumber: number = 0;
+    let testData: TestInput;
+    const test = () => {
+        ++testNumber;
+        //const message = errorMessage ? "fail to reset password" : "reset password successfully";
+        const inputCopy = testData; // Original may be updated by later test case before running
+
+        // // Check if the call passes or fails as desired
+        // if (errorMessage) {
+        //     try { // @ts-ignore
+        //         return testEnv.wrap(resetPassword)(data)
+        //             .then(() => { throw new Error("API call should fail"); })
+        //             .catch((err: any) => assert.equal(err.message, errorMessage));
+        //     } catch (err) { // @ts-ignore
+        //         assert.equal(err.message, errorMessage);
+        //         return;
+        //     }
+        // }
+
+        return (
+            describe(`#${testNumber}: ` + inputCopy.description, () => {
+                it("reset password successfully", () =>
+                    callOnCallFunction("resetPassword", inputCopy)
+                        .then((result) => {
+                            expect(result.data).to.equal(`Password reset email created for ${inputCopy.email}`);
+                        })
+                    // // @ts-ignore
+                    // return testEnv.wrap(resetPassword)(data).then(async (result: string) => {
+                    //     assert.equal(result, `Password reset email created for ${input.email}`,
+                    //         "Response message does not match expected");
+                    // });
+                )
+            })
+        );
+    }
 
     testData = {
         description: "Dummy learner email",
@@ -64,44 +62,65 @@ describe('Success cases for resetPassword endpoint...', () => {
 
 describe('Failure cases for resetPassword endpoint...', () => {
 
-    testNumber = 0;
+    interface TestInput {
+        description: string,
+        email: any,
+    }
+
+    let testNumber: number = 0;
     let testData: TestInput;
 
-    const test = (errMsg: string) => testResetPassword(testData, errMsg);
+    const test = (errMsg: string, errCode: string) => {
+        ++testNumber;
+        const inputCopy = testData; // Original may be updated by later test case before running
+
+        return (
+            describe(`#${testNumber}: ` + inputCopy.description, () => {
+                it("fail to reset password", () =>
+                    callOnCallFunction("resetPassword", inputCopy)
+                        .then(() => { throw new Error("API call should fail"); })
+                        .catch((err: any) => {
+                            expect(err.code).to.equal(errCode);
+                            expect(err.message).to.equal(errMsg);
+                        })
+                )
+            })
+        );
+    }
 
     testData = {
         description: "Non-existent email",
         email: "functions_ut_resetPassword_invalid_email@gmail.com",
     };
-    test("Email does not exist or an error occurred");
+    test("Email does not exist or an error occurred", "functions/invalid-argument");
 
     testData = {
         description: "Invalid email #1",
         email: null,
     };
-    test("The parameter email is required");
+    test("Email is required", "functions/invalid-argument");
 
     testData = {
         description: "Invalid email #2",
         email: 12345,
     };
-    test("The parameter email is required");
+    test("Email is required", "functions/invalid-argument");
 
     testData = {
         description: "Invalid email #3",
         email: "",
     };
-    test("The parameter email is required");
+    test("Email is required", "functions/invalid-argument");
 
     testData = {
         description: "Invalid email #4",
         email: "test.at.test.com",
     };
-    test("Email does not exist or an error occurred");
+    test("Email does not exist or an error occurred", "functions/invalid-argument");
 
     testData = {
         description: "Invalid email #5",
         email: "test@@test.com",
     };
-    test("Email does not exist or an error occurred");
+    test("Email does not exist or an error occurred", "functions/invalid-argument");
 });
