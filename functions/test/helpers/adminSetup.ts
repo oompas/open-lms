75729e99a89bdf8935ae7a19b7ac107fdd21598b
Reid Moffat <18rem8@queensu.ca>
2024-02-06 11:40:40 -0500

    admin test setup

--- functions/test/helpers/adminSetup.ts
+++ functions/test/helpers/adminSetup.ts
@@ -1,11 +1,14 @@
-import { initializeApp } from 'firebase-admin/app';
+// @ts-ignore
+import * as serviceAccount from "../serviceAccountKey.json" with { type: "json" };
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
+import admin from 'firebase-admin';
 
 // Get required services
-initializeApp();
+// @ts-ignore
+const adminApp = admin.initializeApp({ credential: admin.credential.cert(serviceAccount) }, "admin-test");
 
-const adminDb = getFirestore();
-const adminAuth = getAuth();
+const adminDb = getFirestore(adminApp);
+const adminAuth = getAuth(adminApp);
 
 export { adminDb, adminAuth };
