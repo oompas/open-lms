4a1862c04cc486a0ff4e330100cc19f3de79843f
Reid Moffat <18rem8@queensu.ca>
2024-03-08 12:16:18 -0500

    hide password

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -1,16 +1,18 @@
 export default function TextField({
     placeholder,
     text,
-    onChange        // onChange function of the state object - pass in the form of: setValue   <- (no brackets)
+    onChange,        // onChange function of the state object - pass in the form of: setValue   <- (no brackets)
+    hidden
 } : {
     placeholder?: string,
     text: string,
-    onChange: any
+    onChange: any,
+    hidden: boolean
 }) {
     return (
         <input 
             className="border-2 border-[#9D1939] px-4 py-2 -mt-2 text-xl rounded-xl "
-            type="text"
+            type={hidden ? "password" : "text"}
             placeholder={placeholder}
             value={text}
             onChange={(e) => onChange(e.target.value)}
