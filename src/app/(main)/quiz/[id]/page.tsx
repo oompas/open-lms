e5113a188882e7bf144213fffe036d5e39d95715
Reid Moffat <18rem8@queensu.ca>
2024-03-11 14:57:27 -0400

    empty sa isn't complete

--- src/app/(main)/quiz/[id]/page.tsx
+++ src/app/(main)/quiz/[id]/page.tsx
@@ -104,7 +104,7 @@ export default function Quiz({ params }: { params: { id: string } }) {
                         <div className="flex-grow border-4 border-gray-300 mb-2 p-4 rounded-2xl duration-100 flex items-center">
                             <div className="text-2xl">Q{key + 1}</div>
                                 { /* @ts-ignore */ }
-                                {userAnswers[key + 1] !== undefined && (
+                                {(userAnswers[key + 1] !== undefined && userAnswers[key + 1] !== "") && (
                                     <div className="ml-2">
                                         <MdCheckCircleOutline size={24} />
                                     </div>
