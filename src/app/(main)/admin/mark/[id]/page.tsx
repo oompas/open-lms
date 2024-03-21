7a3fc2926cd3e6e091daf14e49d49a58367635cc
Reid Moffat <18rem8@queensu.ca>
2024-03-20 20:26:31 -0400

    load data

--- src/app/(main)/admin/mark/[id]/page.tsx
+++ src/app/(main)/admin/mark/[id]/page.tsx
@@ -2,26 +2,31 @@
 import {useRouter} from "next/navigation";
 import QuizAnswer from "@/app/(main)/admin/mark/[id]/QuizAnswer";
 import Button from "@/components/Button";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useAsync } from "react-async-hook";
+import { callApi } from "@/config/firebase";
 
-export default function Mark() {
+export default function Mark({ params }: { params: { id: string } }) {
 
-    const router = useRouter()
+    const router = useRouter();
 
-    const TEMP_QUIZ_ANSWER_DATA = [
-        { question: "Example knowledge quiz question 1?", answer: "Quiz answer 3", marks: 4, marksEarned: null,  id: 1 },
-        { question: "Example knowledge quiz question 1?", answer: "Quiz answer 3", marks: 4, marksEarned: null,  id: 2 },
-        { question: "Example knowledge quiz question 1?", answer: "Quiz answer 3", marks: 4, marksEarned: null,  id: 3 },
-        { question: "Example knowledge quiz question 1?", answer: "Quiz answer 3", marks: 4, marksEarned: null,  id: 4 }
-    ]
-    const [questions, setQuestions] = useState(TEMP_QUIZ_ANSWER_DATA);
+    const quizQuestions = useAsync(() =>
+        callApi('getQuizToMark')({ quizAttemptId: params.id }) // @ts-ignore
+            .then((rsp) => { setQuestions(rsp.data); return rsp; }),
+        []);
+
+    const [questions, setQuestions] = useState(null);
     const [marks, setMarks] = useState<any[]>([]);
     const [total, setTotal] = useState(0);
     const [score, setScore] = useState(0);
 
     useEffect(() => {
-        var temp_marks: any[] = [];
-        var temp = 0;
+        if (!questions) {
+            return;
+        }
+
+        const temp_marks: any[] = [];
+        let temp = 0;
         questions.map((q, i) => {
             temp_marks.push(0)
             temp += q.marks
@@ -41,6 +46,23 @@ export default function Mark() {
         setScore(temp_sum)
     }
 
+    const loadingPopup = () => {
+        if (quizQuestions.result?.data) {
+            return <></>;
+        }
+
+        return (
+            <div
+                className="fixed flex justify-center items-center w-[100vw] h-[100vh] top-0 left-0 bg-white bg-opacity-50">
+                <div className="flex flex-col w-1/2 bg-white p-12 rounded-xl text-lg shadow-xl">
+                    <div className="text-lg mb-2">
+                        {quizQuestions.loading ? "Loading quiz questions..." : "Error loading quiz questions"}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
     return (
         <main className="flex h-auto w-full mb-4 justify-between">
             <div className="flex flex-col w-[75%] overflow-y-scroll sm:no-scrollbar">
@@ -52,7 +74,7 @@ export default function Mark() {
                     </div>
                 </div>
                 <div className="flex flex-col">
-                {questions.map((question, key) => (
+                {questions && questions.map((question, key) => (
                         <QuizAnswer
                             key={key}
                             index={key}
@@ -74,6 +96,7 @@ export default function Mark() {
                 </div>
                 <Button text="Submit Graded Quiz" onClick={() => router.push('/home')} filled style="mt-auto" />
             </div>
+            {loadingPopup()}
         </main>
     )
 }
