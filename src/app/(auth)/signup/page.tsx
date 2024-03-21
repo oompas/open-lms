d08d4555b8ababef29497110254d07731c777318
Sara-Hall <sara.hall@lifenotes.ca>
2024-03-20 20:38:08 -0400

    Added error messages for invalid name/email/password

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -10,10 +10,41 @@ export default function SignUpPage() {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [isInvalidName, setInvalidName] = useState(false);
+    const [isInvalidEmail, setInvalidEmail] = useState(false);
+    const [isInvalidPass, setInvalidPass] = useState(false);
 
     const signUp = async () => {
+        setInvalidPass(false);
+        setInvalidName(false);
+        setInvalidEmail(false);
+        const hasUpperCase = /[A-Z]/;
+        const hasLowerCase = /[a-z]/;
+        const hasNumbers = /[0-9]/;
+        const hasSpecialChars = /[!#$%&@?]/;
+
+        if (!(hasUpperCase.test(password) && hasLowerCase.test(password) && hasNumbers.test(password) && hasSpecialChars.test(password))) {
+            console.error("Password must be at least ten characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character");
+            setInvalidPass(true);
+            return;
+        }
+
+        if (name.length < 1) {
+            console.error("Name must be at least one character long.");
+            setInvalidName(true);
+            return;
+        }
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            console.error("Invalid email format.");
+            setInvalidEmail(true);
+            return;
+        }
+
         try {
             const response = await callApi('createAccount')({
+                name: name,
                 email: email,
                 password: password
             });
@@ -43,6 +74,21 @@ export default function SignUpPage() {
                             setName={setName}
                             showName={true}
                         />
+                        {isInvalidName && (
+                            <p style={{ maxWidth: "300px" }}>
+                                Name must be at least one character long.
+                            </p>
+                        )}
+                        {isInvalidEmail && (
+                            <p style={{ maxWidth: "300px" }}>
+                                Invalid email format.
+                            </p>
+                        )}
+                        {isInvalidPass && (
+                            <p style={{ maxWidth: "300px" }}>
+                                Password must be at least ten characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character.
+                            </p>
+                        )}
                         <Button text="sign up" onClick={signUp} style="mt-4 ml-auto" icon="arrow" filled/>
                     </div>
                     <div>or</div>
