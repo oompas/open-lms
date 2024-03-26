a349bcce403e76b72f1d920144868f2dd6ac909c
Reid Moffat <18rem8@queensu.ca>
2024-03-26 00:23:11 -0400

    fix quiz timer

--- src/app/(main)/quiz/[id]/page.tsx
+++ src/app/(main)/quiz/[id]/page.tsx
@@ -35,7 +35,7 @@ export default function Quiz({ params }: { params: { id: string } }) {
 
         const interval = setInterval(() => setCountDown(Math.floor(quizData.startTime + (60 * quizData.timeLimit) - (Date.now() / 1000))), 1000);
         return () => clearInterval(interval);
-    }, [countdown]);
+    }, [countdown, quizData]);
 
     const loadingPopup = (
         <div
