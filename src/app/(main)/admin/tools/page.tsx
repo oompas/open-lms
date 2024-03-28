5be0c3dac25564ce9d486dd4fa77838c68b8bed8
Reid Moffat <18rem8@queensu.ca>
2024-03-28 01:08:54 -0400

    quiz to mark time

--- src/app/(main)/admin/tools/page.tsx
+++ src/app/(main)/admin/tools/page.tsx
@@ -35,7 +35,7 @@ export default function Tools() {
                     <QuizToMark
                         key={key}
                         title={quiz.courseName}
-                        date={new Date(quiz.timestamp).toLocaleString()}
+                        date={new Date(quiz.timestamp * 1000).toLocaleString()}
                         learner={quiz.userName}
                         id={quiz.quizAttemptId}
                     />
