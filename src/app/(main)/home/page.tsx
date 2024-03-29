283c0ccdd310b108acebdce8c5bd18480f3cdc6f
Graham Carkner <gcarkner@gmail.com>
2024-03-29 15:18:12 -0400

    course status filters

--- src/app/(main)/home/page.tsx
+++ src/app/(main)/home/page.tsx
@@ -4,10 +4,13 @@ import "../../../config/firebase";
 import Button from "@/components/Button";
 import { useRouter } from "next/navigation";
 import { ApiEndpoints, useAsyncApiCall } from "@/config/firebase";
+import { useState } from "react";
 
 export default function Home() {
 
     const router = useRouter();
+
+    const [filters, setFilters] = useState([0, 1, 2, 3, 4]);
 
     const courses = useAsyncApiCall(ApiEndpoints.GetAvailableCourses, {});
 
@@ -23,7 +26,7 @@ export default function Home() {
             return <div className="text-gray-600 text-center">Enroll in courses to get started!</div>
         }
         // @ts-ignore
-        var temp_courses = [...courses.result.data.filter((course: any) => course.status !== 1)]
+        var temp_courses = [...courses.result.data.filter((course: any) => filters.includes(course.status-2))]
         if (temp_courses.length % 3 === 2) {
             temp_courses.push({name: "_placeholder", status: "", description: "", id: 0})
             temp_courses.push({name: "_placeholder", status: "", description: "", id: 0})
@@ -67,11 +70,51 @@ export default function Home() {
             });
     }
 
+    const statusValues = [
+        "To Do",
+        "In Progress",
+        "Awaiting Marking",
+        "Failed",
+        "Completed",
+    ]
+    const statusColors = {
+        0: "#468DF0",
+        1: "#EEBD31",
+        2: "#0fa9bb",
+        3: "#ab0303",
+        4: "#47AD63",
+    }
+
+    const handleUpdateFilter = (key: number) => {
+        var temp = [...filters]
+        if (temp.includes(key)) 
+            temp.splice(temp.indexOf(key), 1)
+        else 
+            temp.push(key)
+        setFilters(temp)
+    }
+
     return (
         <main className="flex w-full justify-center mb-4">
             <div className="flex flex-col bg-white w-full p-12 rounded-2xl shadow-custom">
                 <div className="flex flex-row items-center mb-2">
-                    <div className="text-lg">My Enrolled Courses</div>
+                    <div className="flex flex-row items-center">
+                        <div className="text-lg mr-4">My Enrolled Courses</div>
+                        <div className="flex flex-row space-x-2">
+                            {statusValues.map((value, key) => (
+                                <button
+                                    className="border-2 rounded-full px-4 py-1 cursor-pointer"
+                                    style={{
+                                        borderColor: filters.includes(key) ? statusColors[key] : null,
+                                        opacity: filters.includes(key) ? 1 : 0.5,
+                                    }}
+                                    onClick={() => handleUpdateFilter(key)}
+                                >
+                                    {value}
+                                </button>
+                            ))}
+                        </div>
+                    </div>
                     <Button text="Browse Available Courses" onClick={() => router.push("/course_search")} style="ml-auto" />
                 </div>
                 <div className="flex flex-row flex-wrap justify-between mt-4 overflow-y-scroll sm:no-scrollbar">
