7b2f9374e7e56434f1b69be1982d8027ec1bf48d
oompas <tech@galassi.ca>
2024-03-31 20:41:55 -0400

    Minor copy changes

--- src/app/(main)/course/[id]/page.tsx
+++ src/app/(main)/course/[id]/page.tsx
@@ -82,7 +82,7 @@ export default function Course({ params }: { params: { id: string } }) {
                     {course.quiz &&
                         <div className="flex flex-row items-center mt-2">
                             <Checkbox checked={course.status === 6} setChecked={null} style="mr-3"/>
-                            <div>{"Pass the required quiz"}</div>
+                            <div>{"Pass the quiz"}</div>
                         </div>
                     }
                 </div>
