64b67786f9552641ce41ef0661cc793962d51d6c
Reid Moffat <18rem8@queensu.ca>
2024-05-29 16:12:03 -0400

    generate spring application

---
+++ api/src/test/java/ca/openlms/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.openlms.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
