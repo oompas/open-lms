81258e10c30c3b18962e2bacd5d5a3d4156b87a7
Reid Moffat <18rem8@queensu.ca>
2024-06-08 20:37:28 -0400

    Added newtoonsoft JSON to controlelrs

--- Api/Program.cs
+++ Api/Program.cs
@@ -10,7 +10,7 @@ public class Program
 
         // Add services to the container.
 
-        builder.Services.AddControllers();
+        builder.Services.AddControllers().AddNewtonsoftJson();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
