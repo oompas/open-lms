3adcc338ad4da9bf5cad3bd64f61ea7dd0ddf059
Reid Moffat <18rem8@queensu.ca>
2024-08-27 15:04:33 -0400

    better logging

--- supabase/functions/createAccount/index.ts
+++ supabase/functions/createAccount/index.ts
@@ -4,7 +4,7 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2.0.0";
 Deno.serve(async (req) => {
     const { email, password } = await req.json();
 
-    console.log(`Entering createAccount function with email: ${email}`);
+    console.log(`(1/4) Entering createAccount function with email: ${email}`);
 
     if (!email || !password) {
       return new Response(
@@ -13,14 +13,14 @@ Deno.serve(async (req) => {
       );
     }
 
-    console.log(`Email & password verified`);
+    console.log(`(2/4) Email & password verified`);
 
     const supabaseUrl = Deno.env.get("SUPABASE_URL")!;
     const supabaseServiceRoleKey = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!;
 
     const supabase = createClient(supabaseUrl, supabaseServiceRoleKey);
 
-    console.log(`Client created`);
+    console.log(`(3/4) Client created`);
 
     const { data, error } = await supabase.auth.admin.createUser({ email, password });
 
@@ -29,6 +29,6 @@ Deno.serve(async (req) => {
         return new Response(JSON.stringify({ error: error.message }), { status: 400 });
     }
 
-    console.log(`User created successfully: ${data.user.email}`);
+    console.log(`(4/4) User created successfully: ${data.user.email}`);
     return new Response(JSON.stringify({ user: data.user }), { status: 200 });
 });
