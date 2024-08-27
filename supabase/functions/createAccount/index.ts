c01107b9f7d0f02ed38898a10dd3d158370efe2e
Reid Moffat <18rem8@queensu.ca>
2024-08-27 19:13:08 -0400

    admin client helper

--- supabase/functions/createAccount/index.ts
+++ supabase/functions/createAccount/index.ts
@@ -1,10 +1,10 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts"
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2.0.0";
+import { adminClient } from "../_config/adminClient.ts";
 
 Deno.serve(async (req) => {
     const { email, password } = await req.json();
 
-    console.log(`(1/4) Entering createAccount function with email: ${email}`);
+    console.log(`(1/3) Entering createAccount function with email: ${email}`);
 
     if (!email || !password) {
       return new Response(
@@ -13,22 +13,15 @@ Deno.serve(async (req) => {
       );
     }
 
-    console.log(`(2/4) Email & password verified`);
+    console.log(`(2/3) Email & password verified`);
 
-    const supabaseUrl = Deno.env.get("SUPABASE_URL")!;
-    const supabaseServiceRoleKey = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!;
-
-    const supabase = createClient(supabaseUrl, supabaseServiceRoleKey);
-
-    console.log(`(3/4) Client created`);
-
-    const { data, error } = await supabase.auth.admin.createUser({ email, password });
+    const { data, error } = await adminClient.auth.admin.createUser({ email, password });
 
     if (error) {
         console.log(`Error creating user: ${error.message}`);
         return new Response(JSON.stringify({ error: error.message }), { status: 400 });
     }
 
-    console.log(`(4/4) User created successfully: ${data.user.email}`);
+    console.log(`(3/3) User created successfully: ${data.user.email}`);
     return new Response(JSON.stringify({ user: data.user }), { status: 200 });
 });
