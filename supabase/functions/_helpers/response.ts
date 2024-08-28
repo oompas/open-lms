ee916d8309b78edc99410b7fbcfcfb7afcb9ef82
Reid Moffat <18rem8@queensu.ca>
2024-08-27 22:22:16 -0400

    don't wrap data + proper status

--- supabase/functions/_helpers/response.ts
+++ supabase/functions/_helpers/response.ts
@@ -2,14 +2,12 @@ import { corsHeaders } from "../_config/cors.ts";
 
 const errorResponse = (errorMessage: string) => new Response(
     JSON.stringify({ error: errorMessage }),
-    { headers: { ...corsHeaders, "Content-Type": "application/json" } },
-    { status: 400 }
+    { headers: { ...corsHeaders, "Content-Type": "application/json" }, status: 400 },
 );
 
 const successResponse = (data: any) => new Response(
-    JSON.stringify({ data: data }),
-    { headers: { ...corsHeaders, "Content-Type": "application/json" } },
-    { status: 200 }
+    JSON.stringify(data),
+    { headers: { ...corsHeaders, "Content-Type": "application/json" }, status: 200 }
 );
 
 export { errorResponse, successResponse };
