49e28394aa55049042f5a2bfe9cc6a088d938b42
Reid Moffat <18rem8@queensu.ca>
2024-08-28 12:46:49 -0400

    document client funcs

--- src/config/clientSupabase.ts
+++ src/config/clientSupabase.ts
@@ -7,6 +7,9 @@ const supabaseClient = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
 
+/**
+ * Signs in a user client-side
+ */
 const signIn = async (email: string, password: string) => {
     const { data, error } = await supabaseClient.auth.signInWithPassword({ email, password });
 
@@ -17,7 +20,11 @@ const signIn = async (email: string, password: string) => {
     return { data, error };
 }
 
-const handleLoginStatus = (router: AppRouterInstance, authPage: boolean) => {
+/**
+ * Call on each page to handle login routing (if you're logged in, route to hte homepage, if not, route to the login page)
+ * @param router useRouter() hook instance
+ */
+const handleLoginStatus = (router: AppRouterInstance) => {
     supabaseClient.auth.onAuthStateChange((event, session) => {
         if (event === 'SIGNED_IN') {
             router.push('/home');
