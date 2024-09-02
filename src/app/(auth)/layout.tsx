25f6f1647a856e5c8f061386af546cd830205d4d
Reid Moffat <18rem8@queensu.ca>
2024-09-02 18:59:08 -0400

    inter unecessary

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -2,18 +2,12 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '../globals.css'
 
-const inter = Inter({subsets: ['latin']})
-
 export const metadata: Metadata = {
     title: 'OpenLMS',
     description: 'Open Education, Unified Learning',
 }
 
-export default function RootLayout({
-                                       children,
-                                   }: {
-    children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className="bg-gray-100">{children}</body>
