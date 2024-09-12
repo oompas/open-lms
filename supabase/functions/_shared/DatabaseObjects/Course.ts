9a751160dc284aad2be9cd113c91351da95255c4
Reid Moffat <18rem8@queensu.ca>
2024-09-12 10:11:18 -0400

    shorten init

--- supabase/functions/_shared/DatabaseObjects/Course.ts
+++ supabase/functions/_shared/DatabaseObjects/Course.ts
@@ -43,21 +43,7 @@ class Course extends DatabaseTable {
 
         super.validateData(data);
 
-        this.id = data.id;
-        this.created_at = data.created_at;
-        this.user_id = data.user_id;
-        this.name = data.name;
-        this.description = data.description;
-        this.link = data.link;
-        this.active = data.active;
-        this.version = data.version;
-        this.min_time = data.min_time;
-        this.max_quiz_attempts = data.max_quiz_attempts;
-        this.min_quiz_score = data.min_quiz_score;
-        this.preserve_quiz_question_order = data.preserve_quiz_question_order;
-        this.quiz_time_limit = data.quiz_time_limit;
-        this.total_quiz_marks = data.total_quiz_marks;
-        this.num_quiz_questions = data.num_quiz_questions;
+        Object.assign(this, data);
     }
 }
 
