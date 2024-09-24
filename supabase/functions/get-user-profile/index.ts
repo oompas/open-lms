a1db0b5de5d833a3d48dc0c07846d6146e75af61
Reid Moffat <18rem8@queensu.ca>
2024-09-24 18:59:37 -0400

    query user enrollments, quiz attempts and completed courses

--- supabase/functions/get-user-profile/index.ts
+++ supabase/functions/get-user-profile/index.ts
@@ -1,6 +1,7 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts"
 import { corsHeaders, successResponse } from "../_shared/helpers.ts";
-import { getUserById, verifyAdministrator } from "../_shared/auth.ts";
+import { getUserById } from "../_shared/auth.ts";
+import { getRows } from "../_shared/database.ts";
 
 Deno.serve(async (req) => {
 
@@ -13,6 +14,15 @@ Deno.serve(async (req) => {
     const user = await getUserById(req, userId);
     if (user instanceof Response) return user;
 
+    const enrollments = await getRows({ table: 'enrolled_course', conditions: ['eq', 'user_id', user.id] });
+    if (enrollments instanceof Response) return enrollments;
+
+    const completedCourses = await getRows({ table: 'course_attempt', conditions: [['eq', 'user_id', user.id], ['eq', 'pass', true]] });
+    if (completedCourses instanceof Response) return completedCourses;
+
+    const quizAttempts = await getRows({ table: 'quiz_attempt', conditions: ['eq', 'user_id', user.id] });
+    if (quizAttempts instanceof Response) return quizAttempts;
+
     const userData = {
         name: user.user_metadata.name,
         email: user.email,
@@ -20,9 +30,9 @@ Deno.serve(async (req) => {
         disabled: false,
         signUpDate: user.created_at,
         lastSignIn: user.last_sign_in_at ?? -1,
-        enrolledCourses: [],
-        completedCourses: [],
-        quizAttempts: [],
+        enrolledCourses: enrollments,
+        completedCourses: completedCourses,
+        quizAttempts: quizAttempts,
     };
 
     return successResponse(userData);
