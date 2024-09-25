b21cd3cd408db14e74e43c7e3390fcba225f4ddb
Reid Moffat <18rem8@queensu.ca>
2024-09-24 21:29:26 -0400

    course insights func

---
+++ supabase/functions/get-course-insight-report/index.ts
@@ -0,0 +1,32 @@
+import "jsr:@supabase/functions-js/edge-runtime.d.ts"
+import { corsHeaders, successResponse } from "../_shared/helpers.ts";
+import { verifyAdministrator } from "../_shared/auth.ts";
+import { getRows } from "../_shared/database.ts";
+
+Deno.serve(async (req) => {
+
+    if (req.method === 'OPTIONS') {
+      return new Response('ok', { headers: corsHeaders })
+    }
+
+    const adminStatus = await verifyAdministrator(req);
+    if (adminStatus instanceof Response) return adminStatus;
+
+    const { courseId } = await req.json();
+
+    const courseData = await getRows({ table: 'course', conditions: ['eq', 'id', courseId] });
+    if (courseData instanceof Response) return courseData;
+
+
+    const responseData = {
+        courseName: courseData.name,
+        learners: [],
+        questions: [],
+        numEnrolled: [],
+        numStarted: [],
+        numComplete: [],
+        avgTime: 0, // In seconds
+    }
+
+    return successResponse(responseData);
+});
