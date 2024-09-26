d6f93f4c7c95455a106e9c136869a0f64c0c41c3
Reid Moffat <18rem8@queensu.ca>
2024-09-26 18:07:55 -0400

    disabled api

---
+++ supabase/functions/disable-user/index.ts
@@ -0,0 +1,26 @@
+import "jsr:@supabase/functions-js/edge-runtime.d.ts"
+import { corsHeaders, errorResponse, log, successResponse } from "../_shared/helpers.ts";
+import { verifyAdministrator } from "../_shared/auth.ts";
+import { adminClient } from "../_shared/adminClient.ts";
+
+Deno.serve(async (req) => {
+
+    if (req.method === 'OPTIONS') {
+        return new Response('ok', { headers: corsHeaders });
+    }
+
+    const { userId, active } = await req.json();
+
+    await verifyAdministrator(req);
+
+    const { data, error } = await adminClient.auth.admin.updateUserById(userId, {
+        ban_duration: active ? "none" : "876600h" // Bans for 100 years
+    });
+
+    if (error) {
+        log(`Error banning user: ${error.message}`);
+        return errorResponse(`Error banning user: ${error.message}`);
+    }
+
+    return successResponse(data);
+});
