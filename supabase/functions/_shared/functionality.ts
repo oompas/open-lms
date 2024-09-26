c2c23fd80831a05392bba166c24adfe004beb59c
Reid Moffat <18rem8@queensu.ca>
2024-09-26 11:56:31 -0400

    post mark quiz handler

--- supabase/functions/_shared/functionality.ts
+++ supabase/functions/_shared/functionality.ts
@@ -1,4 +1,6 @@
 import { getRows } from "./database.ts";
+import { adminClient } from "./adminClient.ts";
+import { errorResponse, log } from "./helpers.ts";
 
 enum CourseStatus {
     NOT_ENROLLED = "NOT_ENROLLED",
@@ -58,4 +60,48 @@ const getCourseStatus = async (courseId: number, userId: string): CourseStatus =
     return CourseStatus.IN_PROGRESS;
 }
 
-export { getCourseStatus };
+/**
+ * Given a quiz that's marked handle the status of it in relation to the course attempt (it may cause the attempt to
+ * pass or fail)
+ *
+ * Note the quiz attempt must have all questions marked (can't have unmarked short answers), and the 'score' and
+ * 'pass' fields are defined too. This doesn't update the quiz attempt, it (possibly) updates the course attempt
+ *
+ * @param quizAttemptId ID of the marked quiz attempt to handle
+ */
+const handleMarkedQuiz = async (quizAttemptId: number) => {
+    const quizAttemptQuery = await getRows({ table: 'quiz_attempt', conditions: ['eq', 'id', quizAttemptId] });
+    if (quizAttemptQuery instanceof Response) return quizAttemptQuery;
+    const quizAttempt = quizAttemptQuery[0];
+
+    const courseAttemptQuery = await getRows({ table: 'course_attempt', conditions: ['eq', 'id', quizAttempt.course_attempt_id] });
+    if (courseAttemptQuery instanceof Response) return courseAttemptQuery;
+    const courseAttempt = courseAttemptQuery[0];
+
+    // If the quiz passes, the course attempt passes
+    if (quizAttempt.pass === true) {
+        const { data, error } = await adminClient.from('course_attempt').update({ pass: true }).eq('id', courseAttempt.id);
+
+        if (error) {
+            log(`Error updating course attempt to pass: ${error.message}`);
+            return errorResponse(`Error updating course attempt to pass: ${error.message}`);
+        }
+    }
+
+    // If the quiz attempt fails, check if they're out of attempts (fail the course), otherwise they can try again
+    const courseQuery = await getRows({ table: 'course', conditions: ['eq', 'id', quizAttempt.course_id] });
+    if (courseQuery instanceof Response) return courseQuery;
+    const course = courseQuery[0];
+
+    const maxQuizAttempts = course.max_quiz_attempts;
+    if (quizAttemptQuery.length >= maxQuizAttempts) {
+        const { data, error } = await adminClient.from('course_attempt').update({ pass: false }).eq('id', courseAttempt.id);
+
+        if (error) {
+            log(`Error updating course attempt to failure: ${error.message}`);
+            return errorResponse(`Error updating course attempt to failure: ${error.message}`);
+        }
+    }
+}
+
+export { getCourseStatus, handleMarkedQuiz };
