09e4bab9d2168cc962ebd2a67108d04e3bcb4f04
Reid Moffat <18rem8@queensu.ca>
2024-09-26 15:48:49 -0400

    link to admin course insights

--- src/app/(main)/admin/profile/[id]/IDEnrolled.tsx
+++ src/app/(main)/admin/profile/[id]/IDEnrolled.tsx
@@ -13,7 +13,7 @@ export default function IDEnrolled({
         <div className="flex flex-col border-2 w-[24%] items-center justify-center p-6 rounded-2xl opacity-0"></div>
         :
         <div className="flex flex-col border-2 w-[24%] items-center justify-center p-6 rounded-2xl">
-          <Link href={"/course/"+id} className="flex flex-row items-center hover:opacity-60 text-xl text-center">
+          <Link href={`/admin/course/${id}/insights/`} className="flex flex-row items-center hover:opacity-60 text-xl text-center">
               {title}
               <LuExternalLink className="ml-1" color="rgb(153 27 27" />
           </Link>
