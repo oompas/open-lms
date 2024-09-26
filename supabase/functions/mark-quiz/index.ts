56f5ba8f47537345677a4db7bfff11eba597ab29
Reid Moffat <18rem8@queensu.ca>
2024-09-26 11:45:11 -0400

    mark quiz endpoi nt

---
+++ supabase/functions/mark-quiz/index.ts
@@ -0,0 +1,48 @@
+import "jsr:@supabase/functions-js/edge-runtime.d.ts"
+import { corsHeaders, errorResponse, getCurrentTimestampTz, log, successResponse } from "../_shared/helpers.ts";
+import { adminClient } from "../_shared/adminClient.ts";
+import { verifyAdministrator } from "../_shared/auth.ts";
+import { getRows } from "../_shared/database.ts";
+
+Deno.serve(async (req) => {
+
+    const timestamp = getCurrentTimestampTz();
+
+    if (req.method === 'OPTIONS') {
+        return new Response('ok', { headers: corsHeaders })
+    }
+
+    const userId = await verifyAdministrator(req);
+
+    const { quizAttemptId, marks }: { quizAttemptId: number, marks: { questionAttemptId: number, marksAchieved: number }[] } = await req.json();
+
+    // Mark questions
+    await Promise.all(marks.map(async (mark) => {
+        return await adminClient.from('quiz_question_attempt').update({ marks_achieved: mark.marks }).eq('id', mark.questionAttemptId);
+    }));
+
+    // Update quiz attempt: get total score and check if it passed
+    const courseQuery = await getRows({ table: 'course', conditions: ['eq', 'id', quizAttempt.course_id] });
+    if (courseQuery instanceof Response) return courseQuery;
+    const course = courseQuery[0];
+
+    const quizQuestionAttempts = await getRows({ table: 'quiz_question_attempt', conditions: ['eq', 'quiz_attempt_id', quizAttemptId] });
+    if (quizQuestionAttempts instanceof Response) return quizQuestionAttempts;
+
+    const totalMarks = quizQuestionAttempts.reduce((sum, attempt) => sum + attempt.marks_achieved, 0);
+
+    const update = {
+        marker_id: userId,
+        marking_time: timestamp,
+        pass: totalMarks >= course.min_quiz_score,
+        score: totalMarks
+    };
+    const { data, error } = await adminClient.from('quiz_attempt').update(update).eq('id', quizAttemptId);
+
+    if (error) {
+        log(`Error updating quiz attempt:`);
+        return errorResponse();
+    }
+
+    return successResponse(data);
+});
