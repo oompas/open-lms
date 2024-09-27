8ac5715890f3a1c404b3d2ad2799e56331be84d1
Reid Moffat <18rem8@queensu.ca>
2024-09-27 15:04:58 -0400

    read notifs when querying

--- supabase/functions/get-notifications/index.ts
+++ supabase/functions/get-notifications/index.ts
@@ -2,6 +2,7 @@ import "jsr:@supabase/functions-js/edge-runtime.d.ts"
 import { corsHeaders, successResponse } from "../_shared/helpers.ts";
 import { getRequestUser } from "../_shared/auth.ts";
 import { getRows } from "../_shared/database.ts";
+import { adminClient } from "../_shared/adminClient.ts";
 
 Deno.serve(async (req) => {
 
@@ -13,6 +14,11 @@ Deno.serve(async (req) => {
 
     const notifications = await getRows({ table: 'notification', conditions: ['eq', 'user_id', user.id] });
     if (notifications instanceof Response) return notifications;
+
+    // Mark all as read
+    await Promise.all(notifications.filter((n) => !n.read).map((n) => {
+        return adminClient.from('notification').update({ read: true }).eq('id', n.id);
+    }));
 
     return successResponse(notifications);
 });
