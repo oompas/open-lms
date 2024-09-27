8a37bd1c64650a902a6348d1e87a32c888cb3494
Reid Moffat <18rem8@queensu.ca>
2024-09-26 23:48:44 -0400

    your details not needed

--- src/app/(main)/profile/page.tsx
+++ src/app/(main)/profile/page.tsx
@@ -53,7 +53,6 @@ export default function Profile() {
                     <div>Return To Courses</div>
                 </Link>
 
-                <div className="text-lg mb-4">Your Account Details</div>
                 <div className="flex flex-col h-full">
                     {user?.role && <StatusBadge status={user?.role} style="my-1"/>}
                     <div className="text-2xl font-bold mt-2">{user?.name}</div>
