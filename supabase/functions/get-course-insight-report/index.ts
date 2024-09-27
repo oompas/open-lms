59fdc924d7cde612525267b8be03350893069149
Reid Moffat <18rem8@queensu.ca>
2024-09-26 20:05:27 -0400

    course insights stats

--- supabase/functions/get-course-insight-report/index.ts
+++ supabase/functions/get-course-insight-report/index.ts
@@ -17,14 +17,20 @@ Deno.serve(async (req) => {
     const courseData = await getRows({ table: 'course', conditions: ['eq', 'id', courseId] });
     if (courseData instanceof Response) return courseData;
 
+    const enrollments = await getRows({ table: 'enrolled_course', conditions: ['eq', 'course_id', courseId] });
+    if (enrollments instanceof Response) return enrollments;
+
+    const attempts = await getRows({ table: 'course_attempt', conditions: ['eq', 'course_id', courseId] });
+    if (attempts instanceof Response) return attempts;
+
 
     const responseData = {
         courseName: courseData[0].name,
         learners: [],
         questions: [],
-        numEnrolled: [],
-        numStarted: [],
-        numComplete: [],
+        numEnrolled: enrollments.length,
+        numStarted: attempts.length,
+        numComplete: attempts.filter((attempt) => attempt.pass === true).length,
         avgTime: 0, // In seconds
     };
 
