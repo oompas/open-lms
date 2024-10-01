97d28c95f6c8994a7320cbbc8604a2eaaecde863
Reid Moffat <18rem8@queensu.ca>
2024-09-30 21:34:29 -0400

    update delete notif for all notifs

--- supabase/functions/delete-notification/index.ts
+++ supabase/functions/delete-notification/index.ts
@@ -1,6 +1,7 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts"
 import { corsHeaders, successResponse } from "../_shared/helpers.ts";
 import { adminClient } from "../_shared/adminClient.ts";
+import { getRequestUserId } from "../_shared/auth.ts";
 
 Deno.serve(async (req) => {
 
@@ -8,13 +9,13 @@ Deno.serve(async (req) => {
         return new Response('ok', { headers: corsHeaders });
     }
 
-    const { notificationIds } = await req.json();
+    const { notificationId, deleteAll } = await req.json();
 
-    // Can apply this to an array of notifications or just one notification
-    if (Array.isArray(notificationIds)) {
-        await Promise.all(notificationIds.map((id) => adminClient.from('notification').delete().eq('id', id)));
+    const userId = await getRequestUserId(req);
+    if (deleteAll) {
+        await adminClient.from('notification').update({ deleted: true }).eq('user_id', userId);
     } else {
-        await adminClient.from('notification').delete().eq('id', notificationIds);
+        await adminClient.from('notification').delete().eq('id', notificationId).eq('user_id', userId);
     }
 
     return successResponse(null);
