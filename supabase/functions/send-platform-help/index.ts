aa033a3ab2e8bf1f1491031987010a77e399c6df
Reid Moffat <18rem8@queensu.ca>
2024-10-04 03:01:44 -0400

    platform help content

--- supabase/functions/send-platform-help/index.ts
+++ supabase/functions/send-platform-help/index.ts
@@ -1,5 +1,7 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts"
 import { OptionsRsp, SuccessResponse } from "../_shared/helpers.ts";
+import { sendEmail } from "../_shared/emails.ts";
+import { getRequestUser } from "../_shared/auth.ts";
 
 Deno.serve(async (req) => {
 
@@ -7,7 +9,38 @@ Deno.serve(async (req) => {
         return OptionsRsp();
     }
 
-    const { name } = await req.json();
+    const { feedback } = await req.json();
+
+    const user = await getRequestUser(req);
+
+    const devEmail = "support@open-lms.ca";
+
+    const subject = "OpenLMS Platform Request";
+    const body = `
+        <style>
+            body { background-color: #f9f9f9; }
+        </style>
+        <div style="font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; max-width: 600px; margin: auto; 
+        background-color: #f9f9f9; border: 1px solid #e0e0e0; padding: 20px;">
+            <header style="text-align: center; margin-bottom: 20px;">
+                <img src="https://lh3.googleusercontent.com/drive-viewer/AKGpihaKJ6WNZbIVmwI2H2DhOpcEjPI20dv54xarsGWLL7Dqpr2YdwjoWz1iJbCXDFjyGA4XsIswyuyiBToe8QTA9Mvddj4Dyw=s2560" 
+                alt="OpenLMS Logo" style="max-width: 200px;">
+            </header>
+            <section style="margin-bottom: 20px;">
+                <h2 style="font-size: 24px; color: #333333;">Request from User "${user.user_metadata.name}"</h2>
+                <p style="font-size: 16px; color: #555;">User information: <br> Name: ${user.user_metadata.name} <br> Email: ${user.email} <br> Uid: ${user.id}</p>
+                <p style="font-size: 16px; color: #555;">User Response: ${feedback}</p>
+            </section>
+            <footer style="font-size: 12px; color: #666666; text-align: center;">
+                <p>Best Regards,</p>
+                <p>The OpenLMS Team</p>
+                <p><a href="https://github.com/oompas/open-lms" style="color: #007bff;">Platform Readme</a> | 
+                <a href="https://github.com/oompas/open-lms/blob/main/LICENSE" style="color: #007bff;">Platform License</a></p>
+            </footer>
+        </div>`;
+
+    const emailSend = await sendEmail(devEmail, subject, body);
+    if (emailSend instanceof Response) return emailSend;
 
     return SuccessResponse("Success rsp!");
 });
