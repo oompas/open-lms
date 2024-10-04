6521f2e1815aefb31c75bf75daf21e1c7667750e
Reid Moffat <18rem8@queensu.ca>
2024-10-04 02:30:50 -0400

    platform help stub

---
+++ supabase/functions/send-platform-help/index.ts
@@ -0,0 +1,13 @@
+import "jsr:@supabase/functions-js/edge-runtime.d.ts"
+import { OptionsRsp, SuccessResponse } from "../_shared/helpers.ts";
+
+Deno.serve(async (req) => {
+
+    if (req.method === 'OPTIONS') {
+        return OptionsRsp();
+    }
+
+    const { name } = await req.json();
+
+    return SuccessResponse("Success rsp!");
+});
