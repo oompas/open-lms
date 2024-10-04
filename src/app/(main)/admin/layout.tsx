dbd35f9a1bc9da4254d38b88ee6dda63fc628616
Reid Moffat <18rem8@queensu.ca>
2024-10-04 03:40:22 -0400

    document not defined fix for admin layout

--- src/app/(main)/admin/layout.tsx
+++ src/app/(main)/admin/layout.tsx
@@ -1,16 +1,22 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { useSession } from "@supabase/auth-helpers-react";
+import { useEffect } from "react";
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
 
     const router = useRouter();
     const session = useSession();
 
-    const role = session?.user?.user_metadata?.role;
-    if (document.readyState === 'complete' && role !== 'Admin' && role !== 'Developer') {
-        router.push('/');
-    }
+    // Route user back to learner homepage if they're not an admin/dev
+    useEffect(() => {
+        if (typeof window !== 'undefined' && document.readyState === 'complete') {
+            const role = session?.user?.user_metadata?.role;
+            if (role !== 'Admin' && role !== 'Developer') {
+                router.push('/');
+            }
+        }
+    }, [session, router]);
 
     return (children);
 }
