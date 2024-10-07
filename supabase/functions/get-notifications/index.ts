5da68382ea4a2ea4ec3d836e16e6268c75e4ac94
Reid Moffat <18rem8@queensu.ca>
2024-10-07 19:24:06 -0400

    don't auto read notifs

--- supabase/functions/get-notifications/index.ts
+++ supabase/functions/get-notifications/index.ts
@@ -2,7 +2,6 @@ import "jsr:@supabase/functions-js/edge-runtime.d.ts"
 import { OptionsRsp, SuccessResponse } from "../_shared/helpers.ts";
 import { getRequestUser } from "../_shared/auth.ts";
 import { getRows } from "../_shared/database.ts";
-import { adminClient } from "../_shared/adminClient.ts";
 
 Deno.serve(async (req) => {
 
@@ -14,11 +13,6 @@ Deno.serve(async (req) => {
 
     const notifications = await getRows({ table: 'notification', conditions: [['eq', 'user_id', user.id], ['eq', 'deleted', false]] });
     if (notifications instanceof Response) return notifications;
-
-    // Mark all as read
-    await Promise.all(notifications.filter((n) => !n.read).map((n) => {
-        return adminClient.from('notification').update({ read: true }).eq('id', n.id);
-    }));
 
     return SuccessResponse(notifications);
 });
