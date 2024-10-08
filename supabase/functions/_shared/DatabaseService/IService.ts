628e9b98f1fa96779843992028509f77f05dd94e
Reid Moffat <18rem8@queensu.ca>
2024-10-08 13:15:50 -0400

    query for services

--- supabase/functions/_shared/DatabaseService/IService.ts
+++ supabase/functions/_shared/DatabaseService/IService.ts
@@ -1,19 +1,69 @@
 import { adminClient } from "../adminClient.ts";
 import DatabaseError from "../Error/DatabaseError.ts";
 
+// Filter docs: https://supabase.com/docs/reference/javascript/using-filters
+type Filter = 'eq' | 'neq' | 'gt' | 'gte' | 'lt' | 'lte' | 'like' | 'ilike' | 'is';
+type QueryConditions = [Filter, string, any] | ['null' | 'notnull', string] | ([Filter, string, any] | ['null' | 'notnull', string])[];
+
 class IService {
 
     protected abstract readonly TABLE_NAME: string;
 
     protected constructor() {}
 
-    protected async getAllRows(): Promise<any> {
+    public async getAllRows(): Promise<any> {
         try {
             const data = await adminClient.from(this.TABLE_NAME).select();
             if (rsp.error) {
                 throw rsp.error;
             }
             return rsp.data;
+        } catch (error) {
+            DatabaseError.create();
+        }
+    }
+
+    public async getById(id: number | string) {
+        try {
+            const data = await adminClient.from(this.TABLE_NAME).select().eq('id', id);
+            if (rsp.error) {
+                throw rsp.error;
+            }
+
+            if (rsp.data.length > 0) {
+                throw `More than 1 document in table ${TABLE_NAME} found with the id ${id}`;
+            }
+
+            return rsp.data[0];
+        } catch (error) {
+            DatabaseError.create(error);
+        }
+    }
+
+    public async query(conditions: QueryConditions) {
+        try {
+            // Wrap single conditions in an array for consistency
+            if (conditions.length && !Array.isArray(conditions[0])) {
+                conditions = [conditions];
+            }
+
+            // Setup query
+            const query = adminClient.from(this.TABLE_NAME).select();
+            conditions.forEach(([filter, key, value]) => {
+                if (filter === 'null') {
+                    query.is(key, null);
+                } else if (filter === 'notnull') {
+                    query.not(key, 'is', null);
+                } else {
+                    query[filter](key, value);
+                }
+            });
+
+            const { data, error } = await query;
+            if (error) {
+                throw error;
+            }
+            return data;
         } catch (error) {
             DatabaseError.create();
         }
