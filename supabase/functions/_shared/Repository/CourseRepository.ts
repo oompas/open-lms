444f5937c9b0dea90dd96e9bf69ca5213d704642
Reid Moffat <18rem8@queensu.ca>
2024-10-09 17:47:44 -0400

    removed unused repo files

--- supabase/functions/_shared/Repository/CourseRepository.ts
+++
@@ -1,8 +0,0 @@
-import BaseRepository from "./BaseRepository.ts";
-
-class CourseRepository extends BaseRepository<Course> {
-    protected tableName = 'courses';
-    protected entityClass = Course;
-}
-
-export default CourseRepository;
