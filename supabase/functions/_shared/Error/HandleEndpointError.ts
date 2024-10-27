4fefe0ff8b928969c96adbefba011ffc90c9fb06
Reid Moffat <18rem8@queensu.ca>
2024-10-27 15:30:46 -0400

    old comments

--- supabase/functions/_shared/Error/HandleEndpointError.ts
+++ supabase/functions/_shared/Error/HandleEndpointError.ts
@@ -1,18 +1,3 @@
-// import { InternalError, logErr } from "../helpers.ts";
-// import DatabaseError from "./DatabaseError.ts";
-
-// const HandleEndpointError = async (req: Request, err: any) => {
-//     let message;
-//     if (err instanceof DatabaseError) {
-//         message = err.message || "Internal database error";
-//     } else {
-//         message = "An unexpected error occurred";
-//     }
-//
-//     logErr(`Error caught: ${message}`);
-//     return new InternalError();
-// }
-
 import ValidationError from "./ValidationError.ts";
 import DatabaseError from "./DatabaseError.ts";
 
@@ -27,12 +12,9 @@ const HandleEndpointError = (req: Request, err: any): Promise<Response> => {
         statusCode = 503;
         message = 'Service Unavailable: Database Error';
     } else if (err instanceof Error) {
-        // Log internal details
         console.error(err.name, err.message, err.stack);
-        // Save error to database (this is a placeholder, you need to implement actual database logic)
         await saveErrorToDatabase(err);
     } else {
-        // Unknown error
         console.error('Unknown error:', err);
         await saveErrorToDatabase(err);
     }
