b74a25e043ebe89063455d351dc49dd67e8ef964
Reid Moffat <18rem8@queensu.ca>
2024-10-28 16:42:52 -0400

    get quiz architecture

---
+++ supabase/functions/get-quiz/getQuiz.ts
@@ -0,0 +1,38 @@
+import EdgeFunctionRequest from "../_shared/EdgeFunctionRequest.ts";
+import { getRows } from "../_shared/database.ts";
+
+const getQuiz = async (request: EdgeFunctionRequest) => {
+
+    const { quizAttemptId } = request.getPayload();
+
+    const quizAttemptQuery = await getRows({ table: 'quiz_attempt', conditions: ['eq', 'id', quizAttemptId] });
+    if (quizAttemptQuery instanceof Response) return quizAttemptQuery;
+    const quizAttempt = quizAttemptQuery[0];
+
+    const courseQuery = await getRows({ table: 'course', conditions: ['eq', 'id', quizAttempt.course_id] });
+    if (courseQuery instanceof Response) return courseQuery;
+    const course = courseQuery[0];
+
+    const questions = await getRows({ table: 'quiz_question', conditions: ['eq', 'course_id', course.id] });
+    if (questions instanceof Response) return questions;
+
+    return {
+        courseName: course.name,
+        numAttempts: -1,
+        maxAttempts: course.max_quiz_attempts,
+        timeLimit: course.quiz_time_limit,
+        startTime: new Date(quizAttempt.start_time),
+        questions: questions.map((q) => {
+            return {
+                id: q.id,
+                order: q.question_order,
+                question: q.question,
+                marks: q.marks,
+                type: q.type,
+                answers: q.answers
+            };
+        })
+    }
+}
+
+export default getQuiz;
--- supabase/functions/get-quiz/index.ts
+++ supabase/functions/get-quiz/index.ts
@@ -1,43 +1,25 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts"
-import { OptionsRsp, SuccessResponse } from "../_shared/helpers.ts";
-import { getRows } from "../_shared/database.ts";
+import { HandleEndpointError, OptionsRsp, SuccessResponse } from "../_shared/response.ts";
+import EdgeFunctionRequest from "../_shared/EdgeFunctionRequest.ts";
+import getCourseData from "../get-course-data/getCourseData.ts";
 
-Deno.serve(async (req) => {
+const ENDPOINT_NAME = "get-quiz";
 
-    if (req.method === 'OPTIONS') {
-        return OptionsRsp();
+Deno.serve(async (req: Request) => {
+
+    const request = new EdgeFunctionRequest(ENDPOINT_NAME, req, { courseId: z.string() });
+
+    try {
+        if (req.method === 'OPTIONS') {
+            return OptionsRsp();
+        }
+
+        await request.validateRequest();
+
+        const rsp = await getCourseData(request);
+
+        return SuccessResponse(rsp);
+    } catch (err) {
+        return await HandleEndpointError(request, err);
     }
-
-    const { quizAttemptId } = await req.json();
-
-    const quizAttemptQuery = await getRows({ table: 'quiz_attempt', conditions: ['eq', 'id', quizAttemptId] });
-    if (quizAttemptQuery instanceof Response) return quizAttemptQuery;
-    const quizAttempt = quizAttemptQuery[0];
-
-    const courseQuery = await getRows({ table: 'course', conditions: ['eq', 'id', quizAttempt.course_id] });
-    if (courseQuery instanceof Response) return courseQuery;
-    const course = courseQuery[0];
-
-    const questions = await getRows({ table: 'quiz_question', conditions: ['eq', 'course_id', course.id] });
-    if (questions instanceof Response) return questions;
-
-    const data = {
-        courseName: course.name,
-        numAttempts: -1,
-        maxAttempts: course.max_quiz_attempts,
-        timeLimit: course.quiz_time_limit,
-        startTime: new Date(quizAttempt.start_time),
-        questions: questions.map((q) => {
-            return {
-                id: q.id,
-                order: q.question_order,
-                question: q.question,
-                marks: q.marks,
-                type: q.type,
-                answers: q.answers
-            };
-        })
-    }
-
-    return SuccessResponse(data);
 });
