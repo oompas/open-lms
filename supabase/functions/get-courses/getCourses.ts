fcc544d4bf842624e320ce0e57ca800164eb2ff1
Reid Moffat <18rem8@queensu.ca>
2024-10-28 16:24:41 -0400

    service for courses query

--- supabase/functions/get-courses/getCourses.ts
+++ supabase/functions/get-courses/getCourses.ts
@@ -1,23 +1,25 @@
 import EdgeFunctionRequest from "../_shared/EdgeFunctionRequest.ts";
-import { adminClient } from "../_shared/adminClient.ts";
+import { CourseService } from "../_shared/Service/Services.ts";
 
 const getCourses = async (request: EdgeFunctionRequest) => {
 
     request.log("Getting user ID, all courses, and user's enrollments...");
 
     const userId = request.getRequestUser().id;
-    const { data, error } = await adminClient
-        .from('course')
-        .select(`
-                id,
-                name,
-                description,
-                min_time,
-                quiz_time_limit,
-                enrolled_course(status)
-              `)
-        .eq('active', true)
-        .eq('enrolled_course.user_id', userId);
+    const data = await CourseService
+        .query(`
+            id,
+            name,
+            description,
+            min_time,
+            quiz_time_limit,
+            enrolled_course(status)
+          `,
+            [
+                ['eq', 'active', true],
+                ['eq', 'enrolled_course.user_id', userId]
+            ]
+        );
 
     request.log(`Request user id: '${userId}'. Queried ${data.length} courses`);
 
