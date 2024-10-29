2b775bf2d5340783a91ee734eef41c2a92bbfc96
Reid Moffat <18rem8@queensu.ca>
2024-10-28 21:56:16 -0400

    course enroll arch

---
+++ supabase/functions/course-enrollment/courseEnrollment.ts
@@ -0,0 +1,30 @@
+import EdgeFunctionRequest from "../_shared/EdgeFunctionRequest.ts";
+import { getRows } from "../_shared/database.ts";
+import { adminClient } from "../_shared/adminClient.ts";
+
+const courseEnrollment = async (request: EdgeFunctionRequest) => {
+
+    const userId = request.getRequestUserId();
+    const { courseId } = request.getPayload();
+
+    const course = await getRows({ table: 'enrolled_course', conditions: [['eq', 'user_id', userId], ['eq', 'course_id', courseId]], expectResults: ['range', [0, 1]] });
+    if (course instanceof Response) return course;
+
+    if (course.length === 0) {
+        const { error: error2 } = await adminClient.from('enrolled_course').insert({ user_id: userId, course_id: id });
+
+        if (error2) {
+            throw error2;
+        }
+    } else {
+        const { error: error3 } = await adminClient.from('enrolled_course').delete().eq('user_id', userId).eq('course_id', id);
+
+        if (error3) {
+            throw error3;
+        }
+    }
+
+    return { enrolled: course.length === 0 };
+}
+
+export default courseEnrollment;
--- supabase/functions/course-enrollment/index.ts
+++ supabase/functions/course-enrollment/index.ts
@@ -1,35 +1,23 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts"
-import { ErrorResponse, OptionsRsp, SuccessResponse } from "../_shared/helpers.ts";
-import { adminClient } from "../_shared/adminClient.ts";
-import { getRequestUserId } from "../_shared/auth.ts";
-import { getRows } from "../_shared/database.ts";
+import EdgeFunctionRequest from "../_shared/EdgeFunctionRequest.ts";
+import { OptionsRsp, SuccessResponse, HandleEndpointError } from "../_shared/response.ts";
+import courseEnrollment from "./courseEnrollment.ts";
 
-Deno.serve(async (req) => {
+Deno.serve(async (req: Request) => {
 
-    if (req.method === 'OPTIONS') {
-        return OptionsRsp();
-    }
+    const request = new EdgeFunctionRequest(import.meta.url, req, { courseId: z.string() });
 
-    const userId = await getRequestUserId(req);
-
-    const { id } = await req.json();
-
-    const course = await getRows({ table: 'enrolled_course', conditions: [['eq', 'user_id', userId], ['eq', 'course_id', id]], expectResults: ['range', [0, 1]] });
-    if (course instanceof Response) return course;
-
-    if (course.length === 0) {
-        const { error: error2 } = await adminClient.from('enrolled_course').insert({ user_id: userId, course_id: id });
-
-        if (error2) {
-            return ErrorResponse(error2.message);
+    try {
+        if (req.method === 'OPTIONS') {
+            return OptionsRsp();
         }
-    } else {
-        const { error: error3 } = await adminClient.from('enrolled_course').delete().eq('user_id', userId).eq('course_id', id);
 
-        if (error3) {
-            return ErrorResponse(error3.message);
-        }
+        await request.validateRequest();
+
+        const rsp = await courseEnrollment(request);
+
+        return SuccessResponse(rsp);
+    } catch (err) {
+        return await HandleEndpointError(request, err);
     }
-
-    return SuccessResponse({ enrolled: course.length === 0 });
 });
