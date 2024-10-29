c87a9a982367ceb8b414b3a471a351a453c5c8fb
Reid Moffat <18rem8@queensu.ca>
2024-10-29 13:22:20 -0400

    ban user arch

---
+++ supabase/functions/disable-user/disableUser.ts
@@ -0,0 +1,23 @@
+import EdgeFunctionRequest from "../_shared/EdgeFunctionRequest.ts";
+import { verifyAdministrator } from "../_shared/auth.ts";
+import { adminClient } from "../_shared/adminClient.ts";
+
+const disableUser = async (request: EdgeFunctionRequest) => {
+
+    const { userId, disable } = request.getPayload();
+
+    await verifyAdministrator(req);
+
+    const { data, error } = await adminClient.auth.admin.updateUserById(userId, {
+        ban_duration: disable ? "876600h": "none" // Bans for 100 years
+    });
+
+    if (error) {
+        request.log(`Error banning user: ${error.message}`);
+        throw error;
+    }
+
+    return data;
+}
+
+export default disableUser;
--- supabase/functions/disable-user/index.ts
+++ supabase/functions/disable-user/index.ts
@@ -1,26 +1,15 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts"
-import { ErrorResponse, log, OptionsRsp, SuccessResponse } from "../_shared/helpers.ts";
-import { verifyAdministrator } from "../_shared/auth.ts";
-import { adminClient } from "../_shared/adminClient.ts";
+import { z } from "https://deno.land/x/zod@v3.23.8/mod.ts";
+import EdgeFunctionRequest, { RunParams } from "../_shared/EdgeFunctionRequest.ts";
+import disableUser from "./disableUser.ts";
 
 Deno.serve(async (req) => {
+    const parameters: RunParams = {
+        metaUrl: import.meta.url,
+        req: req,
+        schemaRecord: { courseId: z.string(), disable: z.bool() },
+        endpointFunction: disableUser
+    };
 
-    if (req.method === 'OPTIONS') {
-        return OptionsRsp();
-    }
-
-    const { userId, disable } = await req.json();
-
-    await verifyAdministrator(req);
-
-    const { data, error } = await adminClient.auth.admin.updateUserById(userId, {
-        ban_duration: disable ? "876600h": "none" // Bans for 100 years
-    });
-
-    if (error) {
-        log(`Error banning user: ${error.message}`);
-        return ErrorResponse(`Error banning user: ${error.message}`);
-    }
-
-    return SuccessResponse(data);
+    return await EdgeFunctionRequest.run(parameters);
 });
