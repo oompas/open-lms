bba2a10a5fbcbb1a7212af582ae12cfd5c63d4f1
Reid Moffat <18rem8@queensu.ca>
2024-10-28 21:19:31 -0400

    setup account

--- supabase/functions/setup-account/index.ts
+++ supabase/functions/setup-account/index.ts
@@ -1,42 +1,23 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts"
-import { ErrorResponse, log, OptionsRsp, SuccessResponse } from "../_shared/helpers.ts";
-import { adminClient } from "../_shared/adminClient.ts";
+import EdgeFunctionRequest from "../_shared/EdgeFunctionRequest.ts";
+import { OptionsRsp, SuccessResponse, HandleEndpointError } from "../_shared/response.ts";
+import setupAccount from "./setupAccount.ts";
 
-Deno.serve(async (req) => {
+Deno.serve(async (req: Request) => {
 
-    if (req.method === 'OPTIONS') {
-        return OptionsRsp();
+    const request = new EdgeFunctionRequest(import.meta.url, req, { userId: z.string(), name: z.string() });
+
+    try {
+        if (req.method === 'OPTIONS') {
+            return OptionsRsp();
+        }
+
+        await request.validateRequest();
+
+        const rsp = await setupAccount(request);
+
+        return SuccessResponse(rsp);
+    } catch (err) {
+        return await HandleEndpointError(request, err);
     }
-
-    const { userId, name } = await req.json();
-
-    log(`(1/3) Entering setup-account function with name: ${name}`);
-
-    if (!name) {
-        return ErrorResponse("Name is required");
-    }
-
-    log(`(2/3) Name verified`);
-
-    const { data, error } = await adminClient.auth.admin.getUserById(userId);
-    if (error) {
-        return ErrorResponse(`Error getting user: ${error.message}`);
-    }
-    const userMetadata = data?.user?.user_metadata;
-    log(`User metadata: ${JSON.stringify(userMetadata)}`);
-    if (userMetadata.name || userMetadata.role) {
-        return ErrorResponse(`USer with id ${userId} has already been setup`);
-    }
-
-    const { data: data2, error: error2 } = await adminClient.auth.admin.updateUserById(userId, {
-        user_metadata: { ...userMetadata, role: "Learner", name: name }
-    });
-
-    if (error2) {
-        log(`Error setting up user: ${error2.message}`);
-        return ErrorResponse(error2.message);
-    }
-
-    log(`(3/3) User setup successfully: ${data.user.email}`);
-    return SuccessResponse(data);
 });
---
+++ supabase/functions/setup-account/setupAccount.ts
@@ -0,0 +1,34 @@
+import EdgeFunctionRequest from "../_shared/EdgeFunctionRequest.ts";
+import { adminClient } from "../_shared/adminClient.ts";
+
+const setupAccount = async (request: EdgeFunctionRequest) => {
+
+    const { userId, name } = request.getPayload();
+
+    request.log(`Entering setup-account function with name: ${name}`);
+
+    const { data, error } = await adminClient.auth.admin.getUserById(userId);
+    if (error) {
+        throw new Error(`Error getting user: ${error.message}`);
+    }
+    const userMetadata = data?.user?.user_metadata;
+    request.log(`User metadata: ${JSON.stringify(userMetadata)}`);
+    if (userMetadata.name || userMetadata.role) {
+        throw new Error(`User with id ${userId} has already been setup`);
+    }
+
+    const { data: data2, error: error2 } = await adminClient.auth.admin.updateUserById(userId, {
+        user_metadata: { ...userMetadata, role: "Learner", name: name }
+    });
+
+    if (error2) {
+        request.log(`Error setting up user: ${error2.message}`);
+        throw error2;
+    }
+
+    request.log(`User setup successfully: ${data.user.email}`);
+
+    return data;
+}
+
+export default setupAccount;
