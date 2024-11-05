5887de37a7674c40ee342866f27ad20ddbe6b354
Reid Moffat <18rem8@queensu.ca>
2024-11-05 17:17:12 -0500

    remove invalid UT

--- tests/sanity/index.test.ts
+++ tests/sanity/index.test.ts
@@ -1,11 +1,6 @@
 import { expect } from 'chai';
-import { createAccount } from "../helpers/auth.ts";
 
 suite("Basic test suite - sanity", function() {
-
-    test("Get profile", async function() {
-        await createAccount('get-profile@test.com', 'password12345');
-    });
 
     suite("Sub-suite", () => {
         test("Sub-suite test", () => {
