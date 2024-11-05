575acd3d511aed4788c4b9dd2c2ca1fa3ab0ab35
Reid Moffat <18rem8@queensu.ca>
2024-11-05 15:43:05 -0500

    req name bug

--- supabase/functions/disable-user/disableUser.ts
+++ supabase/functions/disable-user/disableUser.ts
@@ -6,7 +6,7 @@ const disableUser = async (request: EdgeFunctionRequest) => {
 
     const { userId, disable } = request.getPayload();
 
-    await verifyAdministrator(req);
+    await verifyAdministrator(request);
 
     const { data, error } = await adminClient.auth.admin.updateUserById(userId, {
         ban_duration: disable ? "876600h": "none" // Bans for 100 years
