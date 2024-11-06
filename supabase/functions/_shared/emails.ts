e87420420803673fdd10e7afc4a0396ae4577005
Reid Moffat <18rem8@queensu.ca>
2024-11-06 16:39:52 -0500

    email helper

--- supabase/functions/_shared/emails.ts
+++ supabase/functions/_shared/emails.ts
@@ -3,18 +3,31 @@ import { log } from "./helpers.ts";
 
 const resend = new Resend(Deno.env.get("RESEND_API_KEY") as string);
 
-const sendEmail = async (email: string, subject: string, body: string) => {
-    const { error } = await resend.emails.send({
-        from: "OpenLMS Team",
-        to: email,
-        subject: subject,
-        text: body
-    });
+const sendEmail = async (email: string, subject: string, body: string): Promise<void> => {
 
-    if (error) {
-        log(`Error sending email: ${error.message}`);
-        throw new Error(`Error sending email: ${error.message}`);
+    if (!email || !subject || !body) {
+        log(`sendEmail: Email, subject or body is missing: ${subject}, ${body}`);
+        throw new Error(`sendEmail: Email, subject or body is missing`);
     }
-}
+
+    try {
+        const { error } = await resend.emails.send({
+            from: "OpenLMS <info@open-lms.ca>",
+            to: email,
+            subject: subject,
+            text: body
+        });
+
+        if (error) {
+            log(`Error sending email to ${email}: ${error.message}`);
+            throw new Error(`Error sending email: ${error.message}`);
+        }
+
+        log(`Email sent successfully to ${email}`);
+    } catch (err) {
+        log(`Failed to send email due to an unexpected error: ${err.message}`);
+        throw new Error(`Failed to send email: ${err.message}`);
+    }
+};
 
 export { sendEmail };
