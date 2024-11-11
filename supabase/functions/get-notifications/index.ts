d5c0d058a60052b99d36af44721720b2792be30b
Reid Moffat <18rem8@queensu.ca>
2024-11-11 14:09:24 -0500

    get notifications arch

--- supabase/functions/get-notifications/index.ts
+++ supabase/functions/get-notifications/index.ts
@@ -1,23 +1,14 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts"
-import EdgeFunctionRequest from "../_shared/EdgeFunctionRequest.ts";
-import { OptionsRsp, SuccessResponse, HandleEndpointError } from "../_shared/response.ts";
+import EdgeFunctionRequest, { RunParams } from "../_shared/EdgeFunctionRequest.ts";
 import getNotifications from "./getNotifications.ts";
 
 Deno.serve(async (req: Request) => {
+    const parameters: RunParams = {
+        metaUrl: import.meta.url,
+        req: req,
+        schemaRecord: {},
+        endpointFunction: getNotifications
+    };
 
-    const request = new EdgeFunctionRequest(import.meta.url, req, {});
-
-    try {
-        if (req.method === 'OPTIONS') {
-            return OptionsRsp();
-        }
-
-        await request.validateRequest();
-
-        const rsp = await getNotifications(request);
-
-        return SuccessResponse(rsp);
-    } catch (err) {
-        return await HandleEndpointError(request, err);
-    }
+    return await EdgeFunctionRequest.run(parameters);
 });
