0c1b429b74142ac4c627f181a70e85230d91d870
Reid Moffat <18rem8@queensu.ca>
2024-11-13 22:10:34 -0500

    enrollment arch

--- supabase/functions/course-enrollment/index.ts
+++ supabase/functions/course-enrollment/index.ts
@@ -1,24 +1,15 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts"
 import { z } from "npm:zod";
-import EdgeFunctionRequest from "../_shared/EdgeFunctionRequest.ts";
-import { OptionsRsp, SuccessResponse, HandleEndpointError } from "../_shared/response.ts";
+import EdgeFunctionRequest, { RunParams } from "../_shared/EdgeFunctionRequest.ts";
 import courseEnrollment from "./courseEnrollment.ts";
 
 Deno.serve(async (req: Request) => {
+    const parameters: RunParams = {
+        metaUrl: import.meta.url,
+        req: req,
+        schemaRecord: { courseId: z.string() },
+        endpointFunction: courseEnrollment
+    };
 
-    const request = new EdgeFunctionRequest(import.meta.url, req, { courseId: z.string() });
-
-    try {
-        if (req.method === 'OPTIONS') {
-            return OptionsRsp();
-        }
-
-        await request.validateRequest();
-
-        const rsp = await courseEnrollment(request);
-
-        return SuccessResponse(rsp);
-    } catch (err) {
-        return await HandleEndpointError(request, err);
-    }
+    return await EdgeFunctionRequest.run(parameters);
 });
