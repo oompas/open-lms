bbbd84bb2d1ac98ba73d1df1892677a772b060b4
Reid Moffat <18rem8@queensu.ca>
2024-11-13 22:03:23 -0500

    setup account arch

--- supabase/functions/setup-account/index.ts
+++ supabase/functions/setup-account/index.ts
@@ -1,23 +1,14 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts"
-import EdgeFunctionRequest from "../_shared/EdgeFunctionRequest.ts";
-import { OptionsRsp, SuccessResponse, HandleEndpointError } from "../_shared/response.ts";
+import EdgeFunctionRequest, { RunParams } from "../_shared/EdgeFunctionRequest.ts";
 import setupAccount from "./setupAccount.ts";
 
 Deno.serve(async (req: Request) => {
+    const parameters: RunParams = {
+        metaUrl: import.meta.url,
+        req: req,
+        schemaRecord: { userId: z.string(), name: z.string() },
+        endpointFunction: setupAccount
+    };
 
-    const request = new EdgeFunctionRequest(import.meta.url, req, { userId: z.string(), name: z.string() });
-
-    try {
-        if (req.method === 'OPTIONS') {
-            return OptionsRsp();
-        }
-
-        await request.validateRequest();
-
-        const rsp = await setupAccount(request);
-
-        return SuccessResponse(rsp);
-    } catch (err) {
-        return await HandleEndpointError(request, err);
-    }
+    return await EdgeFunctionRequest.run(parameters);
 });
