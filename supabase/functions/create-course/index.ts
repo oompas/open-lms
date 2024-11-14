58e1d02c0eaf05a9167716d94b9e10aae4a6e6f3
Reid Moffat <18rem8@queensu.ca>
2024-11-13 22:11:28 -0500

    create course arch

--- supabase/functions/create-course/index.ts
+++ supabase/functions/create-course/index.ts
@@ -1,23 +1,14 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts"
-import EdgeFunctionRequest from "../_shared/EdgeFunctionRequest.ts";
-import { OptionsRsp, SuccessResponse, HandleEndpointError } from "../_shared/response.ts";
+import EdgeFunctionRequest, { RunParams } from "../_shared/EdgeFunctionRequest.ts";
 import createCourse from "./createCourse.ts";
 
 Deno.serve(async (req: Request) => {
+    const parameters: RunParams = {
+        metaUrl: import.meta.url,
+        req: req,
+        schemaRecord: { course: z.object() },
+        endpointFunction: createCourse
+    };
 
-    const request = new EdgeFunctionRequest(import.meta.url, req, { course: z.object() });
-
-    try {
-        if (req.method === 'OPTIONS') {
-            return OptionsRsp();
-        }
-
-        await request.validateRequest();
-
-        const rsp = await createCourse(request);
-
-        return SuccessResponse(rsp);
-    } catch (err) {
-        return await HandleEndpointError(request, err);
-    }
+    return await EdgeFunctionRequest.run(parameters);
 });
