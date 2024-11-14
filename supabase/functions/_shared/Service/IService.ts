ab0a05b989b1da91a995e9ba139014806c1774a8
Reid Moffat <18rem8@queensu.ca>
2024-11-13 21:44:32 -0500

    format

--- supabase/functions/_shared/Service/IService.ts
+++ supabase/functions/_shared/Service/IService.ts
@@ -6,29 +6,29 @@ import { log } from "../helpers.ts";
 type Filter = 'eq' | 'neq' | 'gt' | 'gte' | 'lt' | 'lte' | 'like' | 'ilike' | 'is';
 type QueryConditions = [Filter, string, any] | ['null' | 'notnull', string] | ([Filter, string, any] | ['null' | 'notnull', string])[];
 
-    abstract class IService {
+abstract class IService {
 
-        private readonly TABLE_NAME: string;
+    private readonly TABLE_NAME: string;
 
-        protected constructor(tblName: string) {
-            this.TABLE_NAME = tblName;
-        }
+    protected constructor(tblName: string) {
+        this.TABLE_NAME = tblName;
+    }
 
-        /**
-         * Gets ALL data in this table
-         */
-        public async getAllRows(): Promise<any> {
-            try {
-                const { data, error } = await adminClient.from(this.TABLE_NAME).select();
-                if (error) {
-                    throw error;
-                }
-                return data;
-            } catch (err) {
-                log(`Error querying database: ${err.message}`);
-                throw new DatabaseError(`Error querying database: ${err.message}`);
+    /**
+     * Gets ALL data in this table
+     */
+    public async getAllRows(): Promise<any> {
+        try {
+            const { data, error } = await adminClient.from(this.TABLE_NAME).select();
+            if (error) {
+                throw error;
             }
+            return data;
+        } catch (err) {
+            log(`Error querying database: ${err.message}`);
+            throw new DatabaseError(`Error querying database: ${err.message}`);
         }
+    }
 
     /**
      * Gets the document that matches the given id (note: this uses the id column which must be unique)
