73cf6cf57071c230c9982ad14dac681d6bc6af85
Reid Moffat <18rem8@queensu.ca>
2024-11-13 21:59:56 -0500

    get profile arch

--- supabase/functions/get-profile/index.ts
+++ supabase/functions/get-profile/index.ts
@@ -1,23 +1,14 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts"
-import EdgeFunctionRequest from "../_shared/EdgeFunctionRequest.ts";
-import { OptionsRsp, SuccessResponse, HandleEndpointError } from "../_shared/response.ts";
+import EdgeFunctionRequest, { RunParams } from "../_shared/EdgeFunctionRequest.ts";
 import getProfile from "./getProfile.ts";
 
-Deno.serve(async (req) => {
+Deno.serve(async (req: Request) => {
+    const parameters: RunParams = {
+        metaUrl: import.meta.url,
+        req: req,
+        schemaRecord: {},
+        endpointFunction: getProfile
+    };
 
-    const request = new EdgeFunctionRequest(import.meta.url, req, {});
-
-    try {
-        if (req.method === 'OPTIONS') {
-            return OptionsRsp();
-        }
-
-        await request.validateRequest();
-
-        const rsp = await getProfile(request);
-
-        return SuccessResponse(rsp);
-    } catch (err) {
-        return await HandleEndpointError(request, err);
-    }
+    return await EdgeFunctionRequest.run(parameters);
 });
