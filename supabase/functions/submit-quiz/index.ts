ecb4db02371dbff93308bcba7f353d362445c3ad
Reid Moffat <18rem8@queensu.ca>
2024-11-13 22:01:13 -0500

    submit quiz arch

--- supabase/functions/submit-quiz/index.ts
+++ supabase/functions/submit-quiz/index.ts
@@ -1,23 +1,14 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts"
-import EdgeFunctionRequest from "../_shared/EdgeFunctionRequest.ts";
-import { OptionsRsp, SuccessResponse, HandleEndpointError } from "../_shared/response.ts";
+import EdgeFunctionRequest, { RunParams } from "../_shared/EdgeFunctionRequest.ts";
 import submitQuiz from "./submitQuiz.ts";
 
 Deno.serve(async (req: Request) => {
+    const parameters: RunParams = {
+        metaUrl: import.meta.url,
+        req: req,
+        schemaRecord: { quizAttemptId: z.string(), responses: z.array() },
+        endpointFunction: submitQuiz
+    };
 
-    const request = new EdgeFunctionRequest(import.meta.url, req, { quizAttemptId: z.string(), responses: z.array() });
-
-    try {
-        if (req.method === 'OPTIONS') {
-            return OptionsRsp();
-        }
-
-        await request.validateRequest();
-
-        const rsp = await submitQuiz(request);
-
-        return SuccessResponse(rsp);
-    } catch (err) {
-        return await HandleEndpointError(request, err);
-    }
+    return await EdgeFunctionRequest.run(parameters);
 });
