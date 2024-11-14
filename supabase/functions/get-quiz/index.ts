4e4c9b09ac3162ab92511c92efe6ead4c07c486d
Reid Moffat <18rem8@queensu.ca>
2024-11-14 01:20:02 -0500

    called the wrong func

--- supabase/functions/get-quiz/index.ts
+++ supabase/functions/get-quiz/index.ts
@@ -1,14 +1,14 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts";
 import EdgeFunctionRequest, { RunParams } from "../_shared/EdgeFunctionRequest.ts";
-import getCourseData from "../get-course-data/getCourseData.ts";
 import { primaryKeyInt } from "../_shared/validation.ts";
+import getQuiz from "./getQuiz.ts";
 
 Deno.serve(async (req: Request) => {
     const parameters: RunParams = {
         metaUrl: import.meta.url,
         req: req,
         schemaRecord: { quizAttemptId: primaryKeyInt() },
-        endpointFunction: getCourseData
+        endpointFunction: getQuiz
     };
 
     return await EdgeFunctionRequest.run(parameters);
