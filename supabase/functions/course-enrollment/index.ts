55afd40c6943e61bdc750e84261272639c7c8693
Reid Moffat <18rem8@queensu.ca>
2024-11-13 22:09:48 -0500

    zod imports

--- supabase/functions/course-enrollment/index.ts
+++ supabase/functions/course-enrollment/index.ts
@@ -1,4 +1,5 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts"
+import { z } from "npm:zod";
 import EdgeFunctionRequest from "../_shared/EdgeFunctionRequest.ts";
 import { OptionsRsp, SuccessResponse, HandleEndpointError } from "../_shared/response.ts";
 import courseEnrollment from "./courseEnrollment.ts";
