62a1f8fe652995a6ca261a2682a460d98d27cafa
Reid Moffat <18rem8@queensu.ca>
2024-11-15 11:11:33 -0500

    quiz quesiton attempt service

---
+++ supabase/functions/_shared/Service/Impl/QuizQuestionAttemptService.ts
@@ -0,0 +1,8 @@
+import IService from "../IService.ts";
+
+class _quizQuestionAttemptService extends IService {
+
+    TABLE_NAME = "quiz_question_attempt";
+}
+
+export default _quizQuestionAttemptService;
--- supabase/functions/_shared/Service/Services.ts
+++ supabase/functions/_shared/Service/Services.ts
@@ -4,6 +4,7 @@ import _enrollmentService from "./Impl/EnrollmentService.ts";
 import _quizAttemptService from "./Impl/QuizAttemptService.ts";
 import _notificationService from "./Impl/NotificationService.ts";
 import _quizQuestionService from "./Impl/QuizQuestionService.ts";
+import _quizQuestionAttemptService from "./Impl/QuizQuestionAttemptService.ts";
 
 const CourseService = new _courseService();
 const EnrollmentService = new _enrollmentService();
@@ -11,5 +12,6 @@ const CourseAttemptService = new _courseAttemptService();
 const QuizAttemptService = new _quizAttemptService();
 const NotificationService = new _notificationService();
 const QuizQuestionService = new _quizQuestionService();
+const QuizQuestionAttemptService = new _quizQuestionAttemptService();
 
-export { CourseService, EnrollmentService, CourseAttemptService, QuizAttemptService, NotificationService, QuizQuestionService };
+export { CourseService, EnrollmentService, CourseAttemptService, QuizAttemptService, NotificationService, QuizQuestionService, QuizQuestionAttemptService };
