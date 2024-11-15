e48f5f2d53e83bd3111200da53b335eeb5055d2d
Reid Moffat <18rem8@queensu.ca>
2024-11-15 11:13:22 -0500

    clean

--- supabase/functions/_shared/Service/Services.ts
+++ supabase/functions/_shared/Service/Services.ts
@@ -1,17 +1,25 @@
-import _courseAttemptService from "./Impl/CourseAttemptService.ts";
 import _courseService from "./Impl/CourseService.ts";
-import _enrollmentService from "./Impl/EnrollmentService.ts";
-import _quizAttemptService from "./Impl/QuizAttemptService.ts";
 import _notificationService from "./Impl/NotificationService.ts";
+import _enrollmentService from "./Impl/EnrollmentService.ts";
+import _courseAttemptService from "./Impl/CourseAttemptService.ts";
+import _quizAttemptService from "./Impl/QuizAttemptService.ts";
 import _quizQuestionService from "./Impl/QuizQuestionService.ts";
 import _quizQuestionAttemptService from "./Impl/QuizQuestionAttemptService.ts";
 
 const CourseService = new _courseService();
+const NotificationService = new _notificationService();
 const EnrollmentService = new _enrollmentService();
 const CourseAttemptService = new _courseAttemptService();
 const QuizAttemptService = new _quizAttemptService();
-const NotificationService = new _notificationService();
 const QuizQuestionService = new _quizQuestionService();
 const QuizQuestionAttemptService = new _quizQuestionAttemptService();
 
-export { CourseService, EnrollmentService, CourseAttemptService, QuizAttemptService, NotificationService, QuizQuestionService, QuizQuestionAttemptService };
+export {
+    CourseService,
+    NotificationService,
+    EnrollmentService,
+    CourseAttemptService,
+    QuizAttemptService,
+    QuizQuestionService,
+    QuizQuestionAttemptService
+};
