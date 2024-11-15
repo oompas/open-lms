341249224c26c6e7ad9d294d3009d5c3cbc9defc
Reid Moffat <18rem8@queensu.ca>
2024-11-15 11:39:40 -0500

    permission error is 400, not 500

--- supabase/functions/_shared/Error/PermissionError.ts
+++ supabase/functions/_shared/Error/PermissionError.ts
@@ -2,7 +2,7 @@ import ApiError from "./ApiError.ts";
 
 class PermissionError extends ApiError {
     public constructor(message: string) {
-        super(message, "PermissionError", "PERMISSION", 500);
+        super(message, "PermissionError", "PERMISSION", 400);
     }
 }
 
