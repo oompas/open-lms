0a4731857c98e2c26358532f13d5da80aa1ab773
Reid Moffat <18rem8@queensu.ca>
2024-11-15 12:35:07 -0500

    course service query double

--- supabase/functions/get-profile/getProfile.ts
+++ supabase/functions/get-profile/getProfile.ts
@@ -1,9 +1,14 @@
 import EdgeFunctionRequest from "../_shared/EdgeFunctionRequest.ts";
 import { getRows } from "../_shared/database.ts";
+import { CourseAttemptService } from "../_shared/Service/Services.ts";
 
 const getProfile = async (request: EdgeFunctionRequest): Promise<object> => {
 
     const user = request.getRequestUser();
+
+    const c = await CourseAttemptService.query('*, course(*)', [['eq', 'user_id', user.id], ['eq', 'pass', true]]);
+
+    request.log(`course data: ${JSON.stringify(c)}`);
 
     const completedCoursesQuery = await getRows({ table: 'course_attempt', conditions: [['eq', 'user_id', user.id], ['eq', 'pass', true]] });
     if (completedCoursesQuery instanceof Response) return completedCoursesQuery;
