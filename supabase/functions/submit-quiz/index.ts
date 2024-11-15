e4952bbd454b3f7843d1bc514652ebe59a05087e
Reid Moffat <18rem8@queensu.ca>
2024-11-15 12:35:36 -0500

    submit quiz can have sa questions

--- supabase/functions/submit-quiz/index.ts
+++ supabase/functions/submit-quiz/index.ts
@@ -1,7 +1,7 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts";
 import EdgeFunctionRequest, { RunParams } from "../_shared/EdgeFunctionRequest.ts";
 import submitQuiz from "./submitQuiz.ts";
-import { primaryKeyInt, array, object, naturalNumber } from "../_shared/validation.ts";
+import { primaryKeyInt, array, object, naturalNumber, string, union } from "../_shared/validation.ts";
 
 Deno.serve(async (req: Request) => {
     const parameters: RunParams = {
@@ -12,7 +12,7 @@ Deno.serve(async (req: Request) => {
             responses: array(
                 object({
                     questionId: primaryKeyInt(),
-                    answer: naturalNumber()
+                    answer: union([naturalNumber(), string()])
                 })
             )
         },
