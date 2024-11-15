6f572407ab635c99df5d8c0a5982a5f2100a636c
Reid Moffat <18rem8@queensu.ca>
2024-11-15 12:50:24 -0500

    marks achieved must be natural #

--- supabase/functions/mark-quiz-attempt/index.ts
+++ supabase/functions/mark-quiz-attempt/index.ts
@@ -1,13 +1,21 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts";
 import EdgeFunctionRequest, { RunParams } from "../_shared/EdgeFunctionRequest.ts";
 import markQuizAttempt from "./markQuizAttempt.ts";
-import { primaryKeyInt, array, object, number } from "../_shared/validation.ts";
+import { primaryKeyInt, array, object, naturalNumber } from "../_shared/validation.ts";
 
 Deno.serve(async (req: Request) => {
     const parameters: RunParams = {
         metaUrl: import.meta.url,
         req: req,
-        schemaRecord: { quizAttemptId: primaryKeyInt(), marks: array(object({ questionAttemptId: primaryKeyInt(), marksAchieved: number() })) },
+        schemaRecord: {
+            quizAttemptId: primaryKeyInt(),
+            marks: array(
+                object({
+                    questionAttemptId: primaryKeyInt(),
+                    marksAchieved: naturalNumber()
+                })
+            )
+        },
         endpointFunction: markQuizAttempt,
         adminOnly: true
     };
