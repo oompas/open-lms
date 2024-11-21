8f5abd9315bc7df8a861a0232054fe3adaabe92a
Reid Moffat <18rem8@queensu.ca>
2024-11-20 23:47:53 -0500

    cleaned up get profile

--- supabase/functions/get-profile/getProfile.ts
+++ supabase/functions/get-profile/getProfile.ts
@@ -1,33 +1,32 @@
 import EdgeFunctionRequest from "../_shared/EdgeFunctionRequest.ts";
-import { getRows } from "../_shared/database.ts";
 import { CourseAttemptService } from "../_shared/Service/Services.ts";
 
 const getProfile = async (request: EdgeFunctionRequest): Promise<object> => {
 
     const user = request.getRequestUser();
 
-    const c = await CourseAttemptService.query('*, course(*)', [['eq', 'user_id', user.id], ['eq', 'pass', true]]);
+    request.log(`Entering getProfile for user ${user}`);
 
-    request.log(`course data: ${JSON.stringify(c)}`);
+    const completedCourseQuery = await CourseAttemptService.query('id, course_id, end_time, course(name)', [['eq', 'user_id', user.id], ['eq', 'pass', true]]);
 
-    const completedCoursesQuery = await getRows({ table: 'course_attempt', conditions: [['eq', 'user_id', user.id], ['eq', 'pass', true]] });
-    if (completedCoursesQuery instanceof Response) return completedCoursesQuery;
+    request.log(`Successfully queried completed courses: ${JSON.stringify(completedCourseQuery)}`);
 
-    const completedCourses = await Promise.all(completedCoursesQuery.map(async (courseAttempt) => {
-        const course = await getRows({ table: 'course', conditions: ['eq', 'id', courseAttempt.course_id] });
+    const completedCourseData = completedCourseQuery.map((courseAttempt) => {
         return {
             courseId: courseAttempt.id,
-            name: course[0].name,
+            name: courseAttempt.course.name,
             date: courseAttempt.end_time
         };
-    }));
+    });
+
+    request.log(`Parsed completed course data: ${JSON.stringify(completedCourseData)}`);
 
     return {
         name: user.user_metadata.name,
         email: user.email,
         role: user.user_metadata.role,
         signUpDate: user.created_at,
-        completedCourses: completedCourses
+        completedCourses: completedCourseData
     }
 }
 
