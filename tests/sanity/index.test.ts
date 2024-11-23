fd720a20ba9d1a1060a4018485964c4d04597a06
Reid Moffat <18rem8@queensu.ca>
2024-11-23 15:42:40 -0500

    simple api test setup

--- tests/sanity/index.test.ts
+++ tests/sanity/index.test.ts
@@ -1,4 +1,5 @@
 import { expect } from 'chai';
+import { callAPI } from "../helpers/config.ts";
 
 suite("Basic test suite - sanity", function() {
 
@@ -6,5 +7,26 @@ suite("Basic test suite - sanity", function() {
         test("Sub-suite test", () => {
             expect(false).to.equal(false);
         });
+    });
+
+    suite("get-courses", function() {
+
+    });
+
+    suite("get-course-data", function() {
+
+    });
+
+    suite("get-notifications", function() {
+        test("", async function() {
+            const result = await callAPI('get-notifications');
+
+            console.log(`Notifications result: ${JSON.stringify(result)}`);
+            expect(result).to.be.an('array');
+        });
+    });
+
+    suite("get-profile", function() {
+
     });
 });
