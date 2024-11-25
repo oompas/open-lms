536e500f4afdb23fd4d1965a5e960bcd4d4670b3
Reid Moffat <18rem8@queensu.ca>
2024-11-24 21:43:30 -0500

    basic test file

---
+++ tests/sanity/basic.test.ts
@@ -0,0 +1,38 @@
+import { expect } from 'chai';
+import { callAPI } from "../helpers/api.ts";
+import { signIn, signOut } from "../helpers/auth.ts";
+
+suite("Basic endpoints", function() {
+
+    suite("get-courses", function() {
+
+    });
+
+    suite("get-course-data", function() {
+
+    });
+
+    suite("get-notifications", function() {
+
+        const _test = (name: string, expected: []) =>
+            test(name, async function() {
+                const result = await callAPI('get-notifications', {}, false);
+                console.log(`[${name}] Notifications result: ${JSON.stringify(result)}`);
+
+                expect(result).to.be.an('array');
+                expect(result).to.deep.equal(expected);
+            });
+
+        suite("Learner", async function() {
+            _test("No notifications", []);
+        });
+
+        suite("Admin", async function() {
+            _test("No notifications", []);
+        });
+    });
+
+    suite("get-profile", function() {
+
+    });
+});
