66bed0bbc4e16e9568087199304d6d74a7ad8700
Reid Moffat <18rem8@queensu.ca>
2024-11-25 00:45:49 -0500

    logging improvements

--- supabase/functions/get-course-insight-report/getCourseInsightReport.ts
+++ supabase/functions/get-course-insight-report/getCourseInsightReport.ts
@@ -7,16 +7,32 @@ import {
     EnrollmentService, QuizAttemptService,
     QuizQuestionService
 } from "../_shared/Service/Services.ts";
+import { CourseStatus } from "../_shared/Enum/CourseStatus.ts";
 
 const getCourseInsightReport = async (request: EdgeFunctionRequest) => {
 
     const { courseId } = request.getPayload();
+
+    request.log(`Entering getCourseInsightReport with course id ${courseId}`);
 
     const courseData = await CourseService.getById(courseId);
     const enrollments = await EnrollmentService.query('*', ['eq', 'course_id', courseId]);
     const quizQuestions = await QuizQuestionService.query('*', ['eq', 'course_id', courseId]);
     const courseAttempts = await CourseAttemptService.query('*', ['eq', 'course_id', courseId]);
     const quizAttempts = await QuizAttemptService.query('*', ['eq', 'course_id', courseId]);
+
+    request.log(`Queried course, enrollment, and quiz data`);
+
+    const numEnrolled = enrollments.length;
+    const numStarted = enrollments.filter((enrollment) => enrollment.status != CourseStatus.ENROLLED).length;
+    const numCompleted = enrollments.filter((enrollment) => enrollment.status === CourseStatus.COMPLETED).length;
+
+    const completedAttempts = courseAttempts.filter((attempt) => attempt.pass == true);
+    const averageTime = completedAttempts.reduce((sum, attempt) => {
+        return sum + (new Date(attempt.end_time) - new Date(attempt.start_time));
+    }, 0) / completedAttempts.length / 1000; // Time in seconds
+
+    request.log(`Course has ${numEnrolled} enrollments, ${numStarted} started users, ${numCompleted} completions, and an average completion time of ${averageTime} seconds`);
 
     const learnerData = await Promise.all(enrollments.map(async (enrollment) => {
         const user = await request.getUserById(enrollment.user_id);
@@ -46,6 +62,8 @@ const getCourseInsightReport = async (request: EdgeFunctionRequest) => {
         };
     }));
 
+    request.log(`Learner data constructed for ${learnerData.length} users`);
+
     const questionData = quizQuestions.map((question) => {
         return {
             question: question.question,
@@ -54,19 +72,17 @@ const getCourseInsightReport = async (request: EdgeFunctionRequest) => {
         };
     });
 
-    const completedAttempts = courseAttempts.filter((attempt) => attempt.pass === true);
-    const averageTime = completedAttempts.reduce((sum, attempt) => {
-        return sum + (new Date(attempt.end_time) - new Date(attempt.start_time));
-    }, 0) / completedAttempts.length / 1000; // Time in seconds
+    request.log(`Question data constructed for ${questionData.length} questions`);
 
     return {
         courseName: courseData.name,
+        numEnrolled: numEnrolled,
+        numStarted: numStarted,
+        numComplete: numCompleted,
+        avgTime: averageTime,
+
         learners: learnerData,
-        questions: questionData,
-        numEnrolled: enrollments.length,
-        numStarted: courseAttempts.length,
-        numComplete: completedAttempts.length,
-        avgTime: averageTime
+        questions: questionData
     };
 }
 
