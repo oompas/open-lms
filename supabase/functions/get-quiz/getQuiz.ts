126e84e74929814a94eba5fb98bc43226c9e404b
Reid Moffat <18rem8@queensu.ca>
2024-11-25 13:43:01 -0500

    logging for quiz data

--- supabase/functions/get-quiz/getQuiz.ts
+++ supabase/functions/get-quiz/getQuiz.ts
@@ -5,13 +5,32 @@ const getQuiz = async (request: EdgeFunctionRequest): Promise<object> => {
 
     const { quizAttemptId } = request.getPayload();
 
+    request.log(`Entering getQuiz for quiz attempt id ${quizAttemptId}`);
+
     const quizAttempt = await QuizAttemptService.getById(quizAttemptId);
+
+    request.log(`Queried quiz attempt: ${JSON.stringify(quizAttempt)}`);
 
     const [allQuizAttempts, course, quizQuestions] = await Promise.all([
         QuizAttemptService.query('*', ['eq', 'course_attempt_id', quizAttempt.course_attempt_id]),
         CourseService.getById(quizAttempt.course_id),
         QuizQuestionService.query('*', ['eq', 'course_id', quizAttempt.course_id])
     ]);
+
+    request.log(`Queried ${allQuizAttempts.length} quiz attempts, the respective course, and ${quizQuestions.length}`);
+
+    const quizQuestionData = quizQuestions.map((q) => {
+        return {
+            id: q.id,
+            order: q.question_order,
+            question: q.question,
+            marks: q.marks,
+            type: q.type,
+            answers: q.answers
+        };
+    });
+
+    request.log(`Constructed quiz question data for ${quizQuestionData.length} quiz questions`);
 
     return {
         courseName: course.name,
@@ -20,16 +39,7 @@ const getQuiz = async (request: EdgeFunctionRequest): Promise<object> => {
         timeLimit: course.quiz_time_limit,
         startTime: new Date(quizAttempt.start_time),
 
-        questions: quizQuestions.map((q) => {
-            return {
-                id: q.id,
-                order: q.question_order,
-                question: q.question,
-                marks: q.marks,
-                type: q.type,
-                answers: q.answers
-            };
-        })
+        questions: quizQuestionData
     }
 }
 
