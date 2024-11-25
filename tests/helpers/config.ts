2f8ebfa570651bafe1e19ca268af1bea6031a9d0
Reid Moffat <18rem8@queensu.ca>
2024-11-24 19:27:47 -0500

    validate test envars

--- tests/helpers/config.ts
+++ tests/helpers/config.ts
@@ -1,9 +1,22 @@
 import { createClient, FunctionsHttpError } from "@supabase/supabase-js";
 
-const supabaseClient = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+const testUrl = process.env.TEST_SUPABASE_URL;
+const testAnonKey = process.env.TEST_SUPABASE_ANON_KEY;
+
+if (!testUrl) {
+    throw new Error(`Test environment URL not found`);
+}
+if (!testAnonKey) {
+    throw new Error(`Test environment URL not found`);
+}
+if (!/https:\/\/[a-z]+.supabase.co/.test(testUrl)) {
+    throw new Error("Invalid test environment URL");
+}
+if (!/[a-zA-Z0-9.-]+/.test(testAnonKey)) {
+    throw new Error("Invalid test environment anon key");
+}
+
+const supabaseClient = createClient(testUrl, testAnonKey);
 
 const callAPI = async (endpoint: string, body: object = {}) => {
     try {
