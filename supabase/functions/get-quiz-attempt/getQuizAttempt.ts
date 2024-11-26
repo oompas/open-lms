1b31f453dd04670f215a4b178195e924a17f08a2
Reid Moffat <18rem8@queensu.ca>
2024-11-25 23:49:37 -0500

    remove getRows from get quiz attempt

--- supabase/functions/get-quiz-attempt/getQuizAttempt.ts
+++ supabase/functions/get-quiz-attempt/getQuizAttempt.ts
@@ -1,26 +1,20 @@
-import { getRows } from "../_shared/database.ts";
 import EdgeFunctionRequest from "../_shared/EdgeFunctionRequest.ts";
+import {
+    CourseAttemptService,
+    QuizAttemptService,
+    QuizQuestionAttemptService,
+    QuizQuestionService
+} from "../_shared/Service/Services.ts";
 
 const getQuizAttempt = async (request: EdgeFunctionRequest) => {
 
     const { quizAttemptId } = request.getPayload();
 
-    const quizAttemptQuery = await getRows({ table: 'quiz_attempt', conditions: ['eq', 'id', quizAttemptId] });
-    if (quizAttemptQuery instanceof Response) return quizAttemptQuery;
-    const quizAttempt = quizAttemptQuery[0];
-
-    const courseQuery = await getRows({ table: 'course', conditions: ['eq', 'id', quizAttempt.course_id] });
-    if (courseQuery instanceof Response) return courseQuery;
-    const course = courseQuery[0];
-
+    const quizAttempt = await QuizAttemptService.getById(quizAttemptId);
+    const courseQuery = await CourseAttemptService.getById(quizAttempt.course_id);
     const user = await request.getUserById(quizAttempt.user_id);
-    if (user instanceof Response) return user;
-
-    const questionAttempts = await getRows({ table: 'quiz_question_attempt', conditions: ['eq', 'quiz_attempt_id', quizAttemptId] });
-    if (questionAttempts instanceof Response) return questionAttempts;
-
-    const questions = await getRows({ table: 'quiz_question', conditions: ['eq', 'course_id', quizAttempt.course_id] });
-    if (questions instanceof Response) return questions;
+    const questionAttempts = await QuizQuestionAttemptService.query('*', ['eq', 'quiz_attempt_id', quizAttemptId]);
+    const questions = await QuizQuestionService.query('*', ['eq', 'course_id', quizAttempt.course_id]);
 
     const saQuestions = questionAttempts.filter((q) => q.type === "SA").map((q) => {
         const question = questions.find((question) => question.id === q.quiz_question_id);
