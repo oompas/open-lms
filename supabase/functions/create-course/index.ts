10b49260b20f45a98bb9fc6f1a57c699aa080cd1
Reid Moffat <18rem8@queensu.ca>
2024-11-26 00:36:07 -0500

    create course zod simplify

--- supabase/functions/create-course/index.ts
+++ supabase/functions/create-course/index.ts
@@ -1,13 +1,13 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts";
-import { z } from "npm:zod@3.23.8";
 import EdgeFunctionRequest, { RunParams } from "../_shared/EdgeFunctionRequest.ts";
 import createCourse from "./createCourse.ts";
+import { object } from "../_shared/validation.ts";
 
 Deno.serve(async (req: Request) => {
     const parameters: RunParams = {
         metaUrl: import.meta.url,
         req: req,
-        schemaRecord: { course: z.object() },
+        schemaRecord: { course: object() },
         endpointFunction: createCourse,
         adminOnly: true
     };
