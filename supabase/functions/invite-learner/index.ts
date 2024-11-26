04cf3aaddd151f74c99cce0e8edb73c2fe41485b
Reid Moffat <18rem8@queensu.ca>
2024-11-26 00:36:46 -0500

    invite learner zod simplify

--- supabase/functions/invite-learner/index.ts
+++ supabase/functions/invite-learner/index.ts
@@ -1,13 +1,13 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts";
-import { z } from "npm:zod@3.23.8";
 import EdgeFunctionRequest, { RunParams } from "../_shared/EdgeFunctionRequest.ts";
 import inviteLearner from "./inviteLearner.ts";
+import { string } from "../_shared/validation.ts";
 
 Deno.serve(async (req: Request) => {
     const parameters: RunParams = {
         metaUrl: import.meta.url,
         req: req,
-        schemaRecord: { email: z.string() },
+        schemaRecord: { email: string() },
         endpointFunction: inviteLearner,
         adminOnly: true
     };
