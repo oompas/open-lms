3ceb1a1a6feabb7c96f97478a4e955f767961fd7
Reid Moffat <18rem8@queensu.ca>
2024-11-26 00:37:46 -0500

    platform help zod simplify

--- supabase/functions/send-platform-help/index.ts
+++ supabase/functions/send-platform-help/index.ts
@@ -1,13 +1,13 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts";
-import { z } from "npm:zod@3.23.8";
 import EdgeFunctionRequest, { RunParams } from "../_shared/EdgeFunctionRequest.ts";
 import sendPlatformHelp from "./sendPlatformHelp.ts";
+import { string } from "../_shared/validation.ts";
 
 Deno.serve(async (req: Request) => {
     const parameters: RunParams = {
         metaUrl: import.meta.url,
         req: req,
-        schemaRecord: { feedback: z.string() },
+        schemaRecord: { feedback: string() },
         endpointFunction: sendPlatformHelp
     };
 
