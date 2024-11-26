7838c41ca4b106504ebe2aaa36e4aad757a8edf2
Reid Moffat <18rem8@queensu.ca>
2024-11-26 00:26:03 -0500

    removed getrows from course enrollment arch

--- supabase/functions/course-enrollment/courseEnrollment.ts
+++ supabase/functions/course-enrollment/courseEnrollment.ts
@@ -1,14 +1,13 @@
 import EdgeFunctionRequest from "../_shared/EdgeFunctionRequest.ts";
-import { getRows } from "../_shared/database.ts";
 import { adminClient } from "../_shared/adminClient.ts";
+import { CourseService } from "../_shared/Service/Services.ts";
 
 const courseEnrollment = async (request: EdgeFunctionRequest) => {
 
     const userId = request.getRequestUserId();
     const { courseId } = request.getPayload();
 
-    const course = await getRows({ table: 'enrolled_course', conditions: [['eq', 'user_id', userId], ['eq', 'course_id', courseId]], expectResults: ['range', [0, 1]] });
-    if (course instanceof Response) return course;
+    const course = await CourseService.query('*', [['eq', 'user_id', userId], ['eq', 'course_id', courseId]]);
 
     if (course.length === 0) {
         const { error: error2 } = await adminClient.from('enrolled_course').insert({ user_id: userId, course_id: courseId });
