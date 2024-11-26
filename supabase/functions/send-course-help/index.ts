d1217618a71c4747cbe2a2411d7853197ea78140
Reid Moffat <18rem8@queensu.ca>
2024-11-26 00:35:41 -0500

    simplify validation for feedback

--- supabase/functions/send-course-help/index.ts
+++ supabase/functions/send-course-help/index.ts
@@ -1,14 +1,13 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts";
-import { z } from "npm:zod@3.23.8";
 import EdgeFunctionRequest, { RunParams } from "../_shared/EdgeFunctionRequest.ts";
 import sendCourseHelp from "./sendCourseHelp.ts";
-import { primaryKeyInt } from "../_shared/validation.ts";
+import { primaryKeyInt, string } from "../_shared/validation.ts";
 
 Deno.serve(async (req: Request) => {
     const parameters: RunParams = {
         metaUrl: import.meta.url,
         req: req,
-        schemaRecord: { courseId: primaryKeyInt(), feedback: z.string() },
+        schemaRecord: { courseId: primaryKeyInt(), feedback: string() },
         endpointFunction: sendCourseHelp
     };
 
