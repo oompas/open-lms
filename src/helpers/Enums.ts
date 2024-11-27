da989a523906c83beaa29002c7337e2ffc105796
Reid Moffat <18rem8@queensu.ca>
2024-11-26 23:06:30 -0500

    FE enums

---
+++ src/helpers/Enums.ts
@@ -0,0 +1,14 @@
+export enum CourseStatus {
+    NOT_ENROLLED = "NOT_ENROLLED",
+    ENROLLED = "ENROLLED",
+    IN_PROGRESS = "IN_PROGRESS",
+    AWAITING_MARKING = "AWAITING_MARKING",
+    FAILED = "FAILED",
+    COMPLETED = "COMPLETED"
+}
+
+export enum QuestionType {
+    MC = "MC",
+    TF = "TF",
+    SA = "SA"
+}
