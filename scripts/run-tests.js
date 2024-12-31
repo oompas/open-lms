f1e9892981a53fc40472891d043f66a0c09412c6
Reid Moffat <18rem8@queensu.ca>
2024-12-30 21:44:44 -0500

    standardize test run script to lower case

--- scripts/run-tests.js
+++ scripts/run-tests.js
@@ -3,9 +3,9 @@ import { execSync } from 'child_process';
 
 config({ path: '.env.local' });
 
-const folder = process.argv[2].toUpperCase();
-if (folder !== 'SANITY' && folder !== 'DETAILED') {
-    console.error('Error: Folder name must be either "SANITY" or "DETAILED".');
+const folder = process.argv[2].toLowerCase();
+if (folder !== 'sanity' && folder !== 'detailed') {
+    console.error('Error: Folder name must be either "sanity" or "detailed" (case insensitive)');
     process.exit(1);
 }
 
