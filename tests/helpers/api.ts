ea0d4df92f7ca7f865a055074bb397e19524a4e7
Reid Moffat <18rem8@queensu.ca>
2025-04-02 11:19:41 -0400

    call api documentation

--- tests/helpers/api.ts
+++ tests/helpers/api.ts
@@ -1,8 +1,16 @@
 import { supabaseClient } from "./config.ts";
 import { pollAccessToken } from "./auth.ts";
 
-const callAPI = async (endpoint: string, body: object, admin: boolean) => {
+/**
+ * Calls a Supabase API endpoint
+ *
+ * @param endpoint Endpoint name (e.g. get-courses)
+ * @param body Request body (e.g. { userId: faf456ae-68b3-4b71-8f54-52010844b6b3 })
+ * @param admin True to sign in with admin test account, false to sign in with learner test account
+ */
+const callAPI = async (endpoint: string, body: object, admin: boolean): Promise<any> => {
     try {
+        console.log(`Calling API endpoint: ${endpoint} as ${admin ? 'admin' : 'learner'} with body: ${JSON.stringify(body)}`);
         const accessToken = await pollAccessToken(admin);
 
         const options = {
