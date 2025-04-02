9b40b05ebdeef96495a3648a7fd185bf9a48d962
Reid Moffat <18rem8@queensu.ca>
2025-04-02 01:13:24 -0400

    validate all envars

--- tests/helpers/config.ts
+++ tests/helpers/config.ts
@@ -1,5 +1,17 @@
 import { createClient } from "@supabase/supabase-js";
 
+// Add all environment variable names here so they can be validated immediately
+const envars: string[] = [
+    "NEXT_PUBLIC_SUPABASE_URL",
+    "NEXT_PUBLIC_SUPABASE_ANON_KEY",
+    "TEST_SUPABASE_URL",
+    "TEST_SUPABASE_ANON_KEY",
+    "TEST_ADMIN_EMAIL",
+    "TEST_ADMIN_PASSWORD",
+    "TEST_LEARNER_EMAIL",
+    "TEST_LEARNER_PASSWORD"
+];
+
 const getEnvVariable = (key: string): string => {
     const value = process.env[key];
     if (!value) {
@@ -8,9 +20,11 @@ const getEnvVariable = (key: string): string => {
     return value;
 };
 
-console.log("pub: " + getEnvVariable('NEXT_PUBLIC_SUPABASE_URL'));
-console.log("tst: " + getEnvVariable('TEST_SUPABASE_URL'));
+for (const envar of envars) {
+    getEnvVariable(envar); // Throws error if any envars aren't defined
+}
 
+// Setup supabase client
 const supabaseUrl = getEnvVariable('TEST_SUPABASE_URL');
 const supabaseAnonKey = getEnvVariable('TEST_SUPABASE_ANON_KEY');
 
