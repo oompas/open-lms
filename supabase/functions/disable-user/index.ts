e64fcab05acb9c357b3188fe1a219ee7dbe0aa68
Reid Moffat <18rem8@queensu.ca>
2025-04-01 20:10:50 -0400

    user ID is uuid

--- supabase/functions/disable-user/index.ts
+++ supabase/functions/disable-user/index.ts
@@ -1,13 +1,13 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts";
 import EdgeFunctionRequest, { RunParams } from "../_shared/EdgeFunctionRequest.ts";
 import disableUser from "./disableUser.ts";
-import { primaryKeyInt, bool } from "../_shared/validation.ts";
+import { bool, uuid } from "../_shared/validation.ts";
 
 Deno.serve(async (req) => {
     const parameters: RunParams = {
-        metaUrl: import.meta.url,
+        endpointName: import.meta.url,
         req: req,
-        schemaRecord: { userId: primaryKeyInt(), disable: bool() },
+        schemaRecord: { userId: uuid(), disable: bool() },
         endpointFunction: disableUser,
         adminOnly: true
     };
