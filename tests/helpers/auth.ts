3eafba5800bdf773c05499f725fd69b477735fa3
Reid Moffat <18rem8@queensu.ca>
2025-04-02 11:38:09 -0400

    sign in helper documentation

--- tests/helpers/auth.ts
+++ tests/helpers/auth.ts
@@ -1,14 +1,30 @@
 import { getEnvVariable, supabaseClient } from "./config.ts";
 
-const signIn = async (admin: boolean): Promise<object> => {
+interface SignInResponse {
+    session?: {
+        access_token: string;
+    };
+    user?: {
+        id: string;
+        email: string;
+    };
+}
 
-    const email = getEnvVariable(admin ? 'TEST_ADMIN_EMAIL' : 'TEST_LEARNER_EMAIL');
-    const password = getEnvVariable(admin ? 'TEST_ADMIN_PASSWORD' : 'TEST_LEARNER_PASSWORD');
+/**
+ * Signs in to the client with a test account
+ *
+ * @param admin True to sign in with the test admin account, false to sign in with the test learner account
+ */
+const signIn = async (admin: boolean): Promise<SignInResponse> => {
+
+    const userType = admin ? 'ADMIN' : 'LEARNER';
+    const email = getEnvVariable(`TEST_${userType}_EMAIL`);
+    const password = getEnvVariable(`TEST_${userType}_PASSWORD`);
 
     const { data, error } = await supabaseClient.auth.signInWithPassword({ email, password });
 
     if (error) {
-        throw new Error(`Error signing in ${admin ? "admin" : "learner"} test user (/helpers/auth/signIn): ${error.message}`);
+        throw new Error(`Error signing in ${userType} test user (/helpers/auth/signIn): ${error.message}`);
     }
 
     return data;
