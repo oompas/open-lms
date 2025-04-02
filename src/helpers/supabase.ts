8208448010855c3ca4cdb622e7a2059d325c1462
Reid Moffat <18rem8@queensu.ca>
2025-04-02 11:27:57 -0400

    web auto refresh

--- src/helpers/supabase.ts
+++ src/helpers/supabase.ts
@@ -3,7 +3,13 @@ import { createClient, FunctionsHttpError } from "@supabase/supabase-js";
 
 const supabaseClient = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+        auth: {
+            autoRefreshToken: true,
+            persistSession: true
+        }
+    }
 );
 
 /**
