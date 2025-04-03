4f0d71108faf9d9db2697bbf61d56edf71f756be
Reid Moffat <18rem8@queensu.ca>
2025-04-03 11:02:30 -0400

    removed sanity/detailed specification

--- scripts/run-tests.js
+++ scripts/run-tests.js
@@ -1,15 +1,8 @@
 import { execSync } from 'child_process';
 import './load-env.js';
 
-// Verify a valid test folder is specified
-const folder = process.argv[2].toLowerCase();
-if (folder !== 'sanity' && folder !== 'detailed') {
-    console.error('Error: Folder name must be either "sanity" or "detailed" (case insensitive)');
-    process.exit(1);
-}
-
 // Setup and run test command
-const command = `mocha --ui tdd --slow 1000 --timeout 10000 ./tests/${folder}/`;
+const command = `mocha --ui tdd --slow 1000 --timeout 10000 ./tests/`;
 
 try {
     execSync(command, { stdio: 'inherit' });
