04cbcca585d1b1a0148b5f8d392b72dae5265d49
Reid Moffat <18rem8@queensu.ca>
2025-04-02 20:52:45 -0400

    get profile

--- tests/sanity/basic.test.ts
+++ tests/sanity/basic.test.ts
@@ -38,7 +38,6 @@ suite("Sanity Tests - Basic endpoints", function() {
     });
 
     suite("get-notifications", function() {
-
         test("No notifications (learner)", async function() {
             const result = await callAPI('get-notifications', {}, false);
             console.log(`Notifications result: ${JSON.stringify(result)}`);
@@ -57,6 +56,32 @@ suite("Sanity Tests - Basic endpoints", function() {
     });
 
     suite("get-profile", function() {
+        test("User profile (learner)", async function() {
+            const result = await callAPI('get-profile', {}, false);
+            console.log(`Profile result: ${JSON.stringify(result)}`);
 
+            expect(result).to.be.an('object');
+            expect(result).to.have.keys(['name', 'email', 'signUpDate', 'role', 'completedCourses']);
+
+            expect(result).to.have.property('name').equal("Testing learner account");
+            expect(result).to.have.property('email').equal("queens.openlms@gmail.com");
+            expect(result).to.have.property('role').equal("Learner");
+            expect(result).to.have.property('signUpDate').equal("2024-11-25T00:05:16.199291Z");
+            expect(result).to.have.property('completedCourses').deep.equal([]);
+        });
+
+        test("User profile (admin)", async function() {
+            const result = await callAPI('get-profile', {}, true);
+            console.log(`Profile result: ${JSON.stringify(result)}`);
+
+            expect(result).to.be.an('object');
+            expect(result).to.have.keys(['name', 'email', 'signUpDate', 'role', 'completedCourses']);
+
+            expect(result).to.have.property('name').equal("Testing admin account");
+            expect(result).to.have.property('email').equal("testing.queens.openlms@gmail.com");
+            expect(result).to.have.property('role').equal("Admin");
+            expect(result).to.have.property('signUpDate').equal("2024-11-25T00:07:57.484637Z");
+            expect(result).to.have.property('completedCourses').deep.equal([]);
+        });
     });
 });
