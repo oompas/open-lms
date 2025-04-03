3195069244840b737a6c20120c55a7d1cafa7db4
Reid Moffat <18rem8@queensu.ca>
2025-04-02 21:11:49 -0400

    test user constants

--- tests/helpers/constants.ts
+++ tests/helpers/constants.ts
@@ -8,14 +8,23 @@ const getEnvVariable = (key: string): string => {
 
 const Constants = {
     envars: {
-        "NEXT_PUBLIC_SUPABASE_URL": getEnvVariable("NEXT_PUBLIC_SUPABASE_URL"),
-        "NEXT_PUBLIC_SUPABASE_ANON_KEY": getEnvVariable("NEXT_PUBLIC_SUPABASE_ANON_KEY"),
-        "TEST_SUPABASE_URL": getEnvVariable("TEST_SUPABASE_URL"),
-        "TEST_SUPABASE_ANON_KEY": getEnvVariable("TEST_SUPABASE_ANON_KEY"),
-        "TEST_ADMIN_EMAIL": getEnvVariable("TEST_ADMIN_EMAIL"),
-        "TEST_ADMIN_PASSWORD": getEnvVariable("TEST_ADMIN_PASSWORD"),
-        "TEST_LEARNER_EMAIL": getEnvVariable("TEST_LEARNER_EMAIL"),
-        "TEST_LEARNER_PASSWORD": getEnvVariable("TEST_LEARNER_PASSWORD")
+        NEXT_PUBLIC_SUPABASE_URL: getEnvVariable("NEXT_PUBLIC_SUPABASE_URL"),
+        NEXT_PUBLIC_SUPABASE_ANON_KEY: getEnvVariable("NEXT_PUBLIC_SUPABASE_ANON_KEY"),
+        TEST_SUPABASE_URL: getEnvVariable("TEST_SUPABASE_URL"),
+        TEST_SUPABASE_ANON_KEY: getEnvVariable("TEST_SUPABASE_ANON_KEY"),
+        TEST_ADMIN_EMAIL: getEnvVariable("TEST_ADMIN_EMAIL"),
+        TEST_ADMIN_PASSWORD: getEnvVariable("TEST_ADMIN_PASSWORD"),
+        TEST_LEARNER_EMAIL: getEnvVariable("TEST_LEARNER_EMAIL"),
+        TEST_LEARNER_PASSWORD: getEnvVariable("TEST_LEARNER_PASSWORD")
+    },
+    users: {
+        LearnerName: "Testing learner account",
+        LearnerEmail: getEnvVariable("TEST_LEARNER_EMAIL"),
+        LearnerSignup: "2024-11-25T00:05:16.199291Z",
+
+        AdminName: "Testing admin account",
+        AdminEmail: getEnvVariable("TEST_ADMIN_EMAIL"),
+        AdminSignup: "2024-11-25T00:07:57.484637Z"
     }
 }
 
