598c353be7bf1df1f679cd4744548a92699c9cff
Reid Moffat <18rem8@queensu.ca>
2025-04-02 20:32:25 -0400

    no courses test

--- tests/sanity/basic.test.ts
+++ tests/sanity/basic.test.ts
@@ -16,7 +16,21 @@ suite("Sanity Tests - Basic endpoints", function() {
 
 
     suite("get-courses", function() {
+        test("No courses (learner)", async function() {
+            const result = await callAPI('get-courses', {}, false);
+            console.log(`Courses result: ${JSON.stringify(result)}`);
 
+            expect(result).to.be.an('array');
+            expect(result).to.deep.equal([]);
+        });
+
+        test("No courses (admin)", async function() {
+            const result = await callAPI('get-courses', {}, true);
+            console.log(`Courses result: ${JSON.stringify(result)}`);
+
+            expect(result).to.be.an('array');
+            expect(result).to.deep.equal([]);
+        });
     });
 
     suite("get-course-data", function() {
