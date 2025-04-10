f78da89d05a7c2c9dfa88d1b7ce19b80a5f49757
Reid Moffat <18rem8@queensu.ca>
2025-04-09 21:28:03 -0400

    merge func

--- supabase/functions/get-profile/getProfile.ts
+++ supabase/functions/get-profile/getProfile.ts
@@ -1,44 +1,108 @@
 import EdgeFunctionRequest from "../_shared/EdgeFunctionRequest.ts";
-import { CourseAttemptService } from "../_shared/Service/Services.ts";
+import { CourseAttemptService, EnrollmentService, QuizAttemptService } from "../_shared/Service/Services.ts";
 
 const getProfile = async (request: EdgeFunctionRequest): Promise<object> => {
 
-    const user = request.getRequestUser();
+    const { userId } = request.getPayload();
 
-    request.log(`Entering getProfile for user ${user}`);
+    if (!userId) {
+        const user = request.getRequestUser();
 
-    const completedCourseQuery = await CourseAttemptService
-        .query(`
+        request.log(`Entering getProfile for user ${user}`);
+
+        const completedCourseQuery = await CourseAttemptService
+            .query(`
             id,
             course_id,
             end_time,
             course(name)
             `,
-            [
-                ['eq', 'user_id', user.id],
-                ['eq', 'pass', true]
-            ]
-        );
+                [
+                    ['eq', 'user_id', user.id],
+                    ['eq', 'pass', true]
+                ]
+            );
 
-    request.log(`Successfully queried completed courses: ${JSON.stringify(completedCourseQuery)}`);
+        request.log(`Successfully queried completed courses: ${JSON.stringify(completedCourseQuery)}`);
 
-    const completedCourseData = completedCourseQuery.map((courseAttempt) => {
+        const completedCourseData = completedCourseQuery.map((courseAttempt) => {
+            return {
+                courseId: courseAttempt.id,
+                name: courseAttempt.course.name,
+                date: courseAttempt.end_time
+            };
+        });
+
+        request.log(`Parsed completed course data: ${JSON.stringify(completedCourseData)}`);
+
         return {
-            courseId: courseAttempt.id,
-            name: courseAttempt.course.name,
-            date: courseAttempt.end_time
+            name: user.user_metadata.name,
+            email: user.email,
+            role: user.user_metadata.role,
+            signUpDate: user.created_at,
+            completedCourses: completedCourseData
+        }
+    }
+
+    request.log(`Entering getProfile (admin) to get the profile of user ${userId}`);
+
+    request.validateAdmin("Getting a user profile for a specific user requires admin permissions");
+
+    const [user, enrollments, completedCourses, quizAttempts] = await Promise.all([
+        request.getUserById(userId),
+        EnrollmentService.query('*, course(*)', ['eq', 'user_id', userId]),
+        CourseAttemptService.query('*', [['eq', 'user_id', userId], ['eq', 'pass', true]]),
+        QuizAttemptService.query('*', [['eq', 'user_id', userId], ['notnull', 'end_time']])
+    ]);
+
+    request.log(`Queried user data, ${enrollments.length} enrollments, ${completedCourses.length} completed course attempts, and ${quizAttempts.length} completed quiz attempts`);
+
+    const enrolledCourseData = enrollments.map((enrollment) => {
+        return {
+            courseId: enrollment.course.id,
+            name: enrollment.course.name
         };
     });
 
-    request.log(`Parsed completed course data: ${JSON.stringify(completedCourseData)}`);
+    request.log(`Built data for ${enrolledCourseData.length} enrolled courses`);
+
+    const completedCourseData = completedCourses.map((courseAttempt) => {
+        const course = enrollments.find((enrollment) => enrollment.course.id === courseAttempt.course_id);
+        return {
+            name: course.name,
+            completionTime: courseAttempt.end_time
+        };
+    });
+
+    request.log(`Built data for ${completedCourseData.length} completed courses`);
+
+    const quizAttemptData = (quizAttempts.map(async (quizAttempt) => {
+        const course = enrollments.find((enrollment) => enrollment.course.id === quizAttempt.course_id);
+        return {
+            id: quizAttempt.id,
+            endTime: quizAttempt.end_time,
+            courseId: quizAttempt.course_id,
+            courseName: course.name,
+            score: quizAttempt.score,
+            maxScore: course.total_quiz_marks
+        };
+    })).sort((a, b) => new Date(b.end_time) - new Date(a.end_time));
+
+    request.log(`Built data for ${quizAttemptData.length} quiz attempts`);
 
     return {
+        userId: user.id,
         name: user.user_metadata.name,
         email: user.email,
         role: user.user_metadata.role,
+        disabled: !!user.banned_until,
         signUpDate: user.created_at,
-        completedCourses: completedCourseData
-    }
+        lastUpdated: user.updated_at ?? -1,
+
+        enrolledCourses: enrolledCourseData,
+        completedCourses: completedCourseData,
+        quizAttempts: quizAttemptData
+    };
 }
 
 export default getProfile;
--- supabase/functions/get-profile/index.ts
+++ supabase/functions/get-profile/index.ts
@@ -1,12 +1,13 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts";
 import EdgeFunctionRequest, { RunParams } from "../_shared/EdgeFunctionRequest.ts";
 import getProfile from "./getProfile.ts";
+import { uuid } from "../_shared/validation.ts";
 
 Deno.serve(async (req: Request) => {
     const parameters: RunParams = {
         metaUrl: import.meta.url,
         req: req,
-        schemaRecord: {},
+        schemaRecord: { userId: uuid().optional() },
         endpointFunction: getProfile
     };
 
