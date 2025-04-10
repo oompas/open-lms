c2cdbacaa5200a405aeeff799ad10f4ca3a74232
Reid Moffat <18rem8@queensu.ca>
2025-04-09 21:07:12 -0400

    import clean

--- supabase/functions/get-user-profile/getUserProfile.ts
+++ supabase/functions/get-user-profile/getUserProfile.ts
@@ -1,7 +1,6 @@
 import EdgeFunctionRequest from "../_shared/EdgeFunctionRequest.ts";
 import {
     CourseAttemptService,
-    CourseService,
     EnrollmentService,
     QuizAttemptService
 } from "../_shared/Service/Services.ts";
@@ -46,7 +45,7 @@ const getUserProfile = async (request: EdgeFunctionRequest) => {
             score: quizAttempt.score,
             maxScore: course.total_quiz_marks
         };
-    })).sort((a, b) =>  new Date(b.end_time) - new Date(a.end_time));
+    })).sort((a, b) => new Date(b.end_time) - new Date(a.end_time));
 
     request.log(`Built data for course enrollments, completed courses and quiz attempts`);
 
