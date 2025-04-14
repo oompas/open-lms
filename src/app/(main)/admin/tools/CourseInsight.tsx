6f402f3f897868bb61daa6e42afa7c0f1d8e31b5
Reid Moffat <18rem8@queensu.ca>
2025-04-14 12:12:13 -0400

    inactive course grey

--- src/app/(main)/admin/tools/CourseInsight.tsx
+++ src/app/(main)/admin/tools/CourseInsight.tsx
@@ -7,6 +7,7 @@ export default function CourseInsight({
     courseData: {
         id: string,
         name: string,
+        active: boolean,
         numEnrolled: number,
         numComplete: number,
         avgTime: number,
@@ -16,7 +17,7 @@ export default function CourseInsight({
     return (
         <tr key={courseData.id} className="border">
             <td className="border p-2">
-                <Link href={"/admin/course/" + courseData.id + "/insights"} className="flex flex-row items-center hover:opacity-60">
+                <Link href={"/admin/course/" + courseData.id + "/insights"} className={`flex flex-row items-center hover:opacity-60 ${!courseData.active && "text-gray-400"}`}>
                     {courseData.name}
                     <LuExternalLink className="ml-1" color="rgb(153 27 27" />
                 </Link>
