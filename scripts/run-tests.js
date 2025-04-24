b1d267e855931392ed09919c0415ad8edbe6430c
Reid Moffat <18rem8@queensu.ca>
2025-04-23 22:02:14 -0400

    mutex for CI

--- scripts/run-tests.js
+++ scripts/run-tests.js
@@ -1,12 +1,6 @@
 import { execSync } from 'child_process';
-import "./TestMutex.js";
+import "./acquire-mutex.js"; // Loads envars & acquires mutex
 
-if (!process.env.IS_SANITY) {
-    throw new Error("Please specify the IS_SANITY envar manually (actions env: or set in your run" +
-        " script/configuration)");
-}
-
-// Setup and run test command
 const command = `mocha --require ts-node/register --ui tdd --slow 1000 --timeout 10000 ./tests/`;
 
 try {
