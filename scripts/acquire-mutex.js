7f6331e42ab6ca6dde8853ae466a692d244d8484
Reid Moffat <18rem8@queensu.ca>
2025-04-24 21:06:16 -0400

    properly parse sanity var

--- scripts/acquire-mutex.js
+++ scripts/acquire-mutex.js
@@ -1,8 +1,9 @@
 import TestMutex from "./TestMutex.js";
 import { environment } from './load-env.js';
 
-const timeout = process.env.IS_SANITY ? 5 : 60;
-const testType = process.env.IS_SANITY ? "SANITY" : "DETAILED";
+const isSanity = process.env.IS_SANITY?.toLowerCase() === 'true';
+const timeout = isSanity ? 5 : 60;
+const testType = isSanity ? "SANITY" : "DETAILED";
 
 // Create mutex helper and acquire it before testing
 const testMutex = new TestMutex(environment, timeout, testType);
