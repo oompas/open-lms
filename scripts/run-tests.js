b22b76c5077bb096fc48bb06aaf7572ff8e2f80a
Reid Moffat <18rem8@queensu.ca>
2025-04-24 20:41:26 -0400

    simplify import

--- scripts/run-tests.js
+++ scripts/run-tests.js
@@ -1,12 +1,7 @@
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
 import { execSync } from 'child_process';
 import "./load-env.js";
 
-// Require mutex file for mocha hooks
-const mutexFilePath = resolve(dirname(fileURLToPath(import.meta.url)), './acquire-mutex.js');
-
-const command = `mocha --require ts-node/register --require ${mutexFilePath} --ui tdd --slow 1000 --timeout 10000 ./tests/`;
+const command = `mocha --require ts-node/register --require './acquire-mutex.js' --ui tdd --slow 1000 --timeout 10000 ./tests/`;
 
 try {
     execSync(command, { stdio: 'inherit' });
