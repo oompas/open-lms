af3367325ef2a605ba553a09bb295aa5b4694659
Reid Moffat <18rem8@queensu.ca>
2025-04-24 20:43:49 -0400

    undo breaking require change

--- scripts/run-tests.js
+++ scripts/run-tests.js
@@ -1,7 +1,12 @@
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
 import { execSync } from 'child_process';
 import "./load-env.js";
 
-const command = `mocha --require ts-node/register --require './acquire-mutex.js' --ui tdd --slow 1000 --timeout 10000 ./tests/`;
+// Require mutex file for mocha hooks
+const mutexFilePath = resolve(dirname(fileURLToPath(import.meta.url)), './acquire-mutex.js');
+
+const command = `mocha --require ts-node/register --require ${mutexFilePath} --ui tdd --slow 1000 --timeout 10000 ./tests/`;
 
 try {
     execSync(command, { stdio: 'inherit' });
