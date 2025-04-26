8ec0bba27a7c0a54cc2c64ccc2e8b4c45f7ddab1
Reid Moffat <18rem8@queensu.ca>
2025-04-25 23:10:33 -0400

    keep alive cron schedule

---
+++ sql/setup/keep_alive_cron.sql
@@ -0,0 +1,20 @@
+/**
+ * Schedules a simple cron job every 6 days to keep the project from pausing
+ */
+
+-- Enable the pg_cron extension if not already
+CREATE EXTENSION IF NOT EXISTS pg_cron;
+
+CREATE OR REPLACE FUNCTION public.keep_alive()
+RETURNS void AS $$
+BEGIN
+  PERFORM 1; -- Simple query to generate activity
+END;
+$$ LANGUAGE plpgsql;
+
+-- Run every 6 days 4/5 AM Eastern (4 in the winter, 5 in the summer)
+SELECT cron.schedule(
+    'keep_project_alive',
+    '0 9 */6 * *',
+    $$SELECT public.keep_alive();$$
+);
