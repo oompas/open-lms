1b610aee1cade893cd567d8c88221ef27020bc6c
Reid Moffat <18rem8@queensu.ca>
2025-05-26 17:57:46 -0400

    strict validation

--- tests/unit/get-course-insight-report.test.ts
+++ tests/unit/get-course-insight-report.test.ts
@@ -10,62 +10,88 @@ suite("getCourseInsightReport", function() {
 
     setupWipeDb();
 
+    // Checks if an object fits the endpoint response structure
+    function validateStructure(result: any, errMsgDetails: string) {
+
+        try {
+            // Validate object structure
+            expect(result).to.be.an('object');
+            expect(result).to.have.all.keys('courseName', 'isActive', 'numEnrolled', 'numStarted', 'numComplete', 'avgTime', 'learners', 'questions');
+
+            // Basic course data validation
+            expect(result.courseName).to.be.a('string');
+            expect(result.isActive).to.be.a('boolean');
+            expect(result.numEnrolled).to.be.a('number').and.to.be.at.least(0).and.to.satisfy(Number.isInteger);
+            expect(result.numStarted).to.be.a('number').and.to.be.at.least(0).and.to.satisfy(Number.isInteger);
+            expect(result.numComplete).to.be.a('number').and.to.be.at.least(0).and.to.satisfy(Number.isInteger);
+            if (result.avgTime !== null) {
+                expect(result.avgTime).to.be.a('number').and.to.be.at.least(0).and.to.satisfy(Number.isInteger);
+            }
+
+            // Learner data validation
+            expect(result.learners).to.be.an('array');
+
+            result.learners.forEach((learner: any) => {
+                expect(learner).to.be.an('object');
+                expect(learner).to.have.all.keys('name', 'userId', 'status', 'latestQuizAttemptId', 'latestQuizAttemptTime');
+
+                expect(learner.name).to.be.a('string');
+                expect(learner.userId).to.be.a('string').and.to.match(/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/);
+                expect(learner.status).to.be.a('string').and.to.be.oneOf(Object.values(CourseStatus));
+
+                // If one quiz field is null, both must logically be null
+                if (learner.latestQuizAttemptId === null || learner.latestQuizAttemptTime === null) {
+                    expect(learner.latestQuizAttemptTime).to.be.null;
+                    expect(learner.latestQuizAttemptId).to.be.null;
+                }
+
+                if (learner.latestQuizAttemptId !== null) {
+                    expect(learner.latestQuizAttemptId).to.be.a('number');
+                    expect(learner.latestQuizAttemptId).to.be.at.least(1);
+                    expect(Number.isInteger(learner.latestQuizAttemptId)).to.be.true;
+                } else {
+                    expect(learner.latestQuizAttemptId).to.be.null;
+                }
+
+                if (learner.latestQuizAttemptTime !== null) {
+                    expect(learner.latestQuizAttemptTime).to.be.a('number');
+                    expect(learner.latestQuizAttemptTime).to.be.at.least(1); // Can't complete a quiz in zero seconds
+                    expect(Number.isInteger(learner.latestQuizAttemptTime)).to.be.true; // Time in milliseconds
+                } else {
+                    expect(learner.latestQuizAttemptTime).to.be.null;
+                }
+            });
+
+            // Question data validation
+            expect(result.questions).to.be.an('array');
+
+            result.questions.forEach((question: any) => {
+                expect(question).to.be.an('object');
+                expect(question).to.have.all.keys('question', 'marks', 'stats');
+
+                expect(question.question).to.be.a('string').and.not.to.be.empty;
+                expect(question.marks).to.be.a('number').and.be.at.least(1).and.satisfy(Number.isInteger);
+                expect(question.stats).to.be.an('object');
+            });
+        } catch (err: any) {
+            throw new Error(errMsgDetails + err.message);
+        }
+    }
+
     /**
      * Runs a test for valid inputs & context, ensuring the endpoint works properly
      */
-    async function validCase(courseId: number): Promise<void> {
+    async function validCase(courseId: number, expectedResult?: object): Promise<void> {
         const result = await callAPI('get-course-insight-report', { courseId }, true);
 
-        // Validate object structure
-        expect(result).to.be.an('object');
-        expect(result).to.have.all.keys('courseName', 'isActive', 'numEnrolled', 'numStarted', 'numComplete', 'avgTime', 'learners', 'questions');
+        if (expectedResult) {
+            validateStructure(expectedResult, "Expected result doesn't fit the required structure: ");
+            validateStructure(result, "API result doesn't fit the required structure: ");
 
-        // Basic course data validation
-        expect(result.courseName).to.be.a('string');
-        expect(result.isActive).to.be.a('boolean');
-        expect(result.numEnrolled).to.be.a('number').and.to.be.at.least(0).and.to.satisfy(Number.isInteger);
-        expect(result.numStarted).to.be.a('number').and.to.be.at.least(0).and.to.satisfy(Number.isInteger);
-        expect(result.numComplete).to.be.a('number').and.to.be.at.least(0).and.to.satisfy(Number.isInteger);
-        if (result.avgTime !== null) {
-            expect(result.avgTime).to.be.a('number').and.to.be.at.least(0).and.to.satisfy(Number.isInteger);
+            expect(result).to.deep.equal(expectedResult);
+        } else {
+            validateStructure(result, "Expected result doesn't fit the required structure: ");
         }
-
-        // Learner data validation
-        expect(result.learners).to.be.an('array');
-
-        result.learners.forEach((learner: any) => {
-            expect(learner).to.be.an('object');
-            expect(learner).to.have.all.keys('name', 'userId', 'status', 'latestQuizAttemptId', 'latestQuizAttemptTime');
-
-            expect(learner.name).to.be.a('string');
-            expect(learner.userId).to.be.a('string').and.to.match(/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/);
-            expect(learner.status).to.be.a('string').and.to.be.oneOf(Object.values(CourseStatus));
-
-            // If one quiz field is null, both must logically be null
-            if (learner.latestQuizAttemptId === null || learner.latestQuizAttemptTime === null) {
-                expect(learner.latestQuizAttemptTime).to.be.null;
-                expect(learner.latestQuizAttemptId).to.be.null;
-            }
-
-            if (learner.latestQuizAttemptId !== null) {
-                expect(learner.latestQuizAttemptId).to.be.a('number');
-                expect(learner.latestQuizAttemptId).to.be.at.least(1);
-                expect(Number.isInteger(learner.latestQuizAttemptId)).to.be.true;
-            } else {
-                expect(learner.latestQuizAttemptId).to.be.null;
-            }
-
-            if (learner.latestQuizAttemptTime !== null) {
-                expect(learner.latestQuizAttemptTime).to.be.a('number');
-                expect(learner.latestQuizAttemptTime).to.be.at.least(1); // Can't complete a quiz in zero seconds
-                expect(Number.isInteger(learner.latestQuizAttemptTime)).to.be.true; // Time in milliseconds
-            } else {
-                expect(learner.latestQuizAttemptTime).to.be.null;
-            }
-        });
-
-        // Question data validation
-        expect(result.questions).to.be.an('array');
     }
 
     /**
