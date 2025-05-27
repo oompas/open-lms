630ac92ad7f4a7bfbbd6ece58e4e6671658058b2
Reid Moffat <18rem8@queensu.ca>
2025-05-27 17:20:00 -0400

    increment method

--- supabase/functions/_shared/Service/Impl/QuizQuestionService.ts
+++ supabase/functions/_shared/Service/Impl/QuizQuestionService.ts
@@ -39,6 +39,48 @@ class _quizQuestionService extends IService {
             throw new Error(`Error adding quiz questions for new course: ${error.message}`);
         }
     }
+
+    /**
+     * Increments answer statistics for each question in a quiz
+     *
+     * @param submittedAnswers Object with question IDs mapping to the selected answer (the answer itself, not itsindex)
+     */
+    public async incrementQuestionStats(submittedAnswers: { [key: number]: string }) {
+
+        // Query current question values
+        const questionIds = Object.keys(submittedAnswers);
+        const { data: questions, error: fetchError } = await adminClient
+            .from(this.TABLE_NAME)
+            .select('id, submitted_answers')
+            .in('id', questionIds)
+            .not('submitted_answers', 'is', null);
+
+        if (fetchError) {
+            throw new Error(`Error fetching question stats: ${fetchError.message}`);
+        }
+
+        // Increment the selected answer locally
+        const updates = questions.map(question => {
+            const selectedAnswer = submittedAnswers[question.id];
+
+            return {
+                id: question.id,
+                submitted_answers: {
+                    ...question.submitted_answers,
+                    [selectedAnswer]: currentStats[selectedAnswer] + 1
+                }
+            };
+        });
+
+        // Updates question database records with the new values
+        const { error: updateError } = await adminClient
+            .from(this.TABLE_NAME)
+            .upsert(updates);
+
+        if (updateError) {
+            throw new Error(`Error updating question stats: ${updateError.message}`);
+        }
+    }
 }
 
 export default _quizQuestionService;
