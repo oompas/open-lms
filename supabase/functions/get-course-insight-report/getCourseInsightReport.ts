88e276e524c49ed7beec2b596782415e0ade8e85
Reid Moffat <18rem8@queensu.ca>
2025-05-26 20:00:03 -0400

    null specify for avg time

--- supabase/functions/get-course-insight-report/getCourseInsightReport.ts
+++ supabase/functions/get-course-insight-report/getCourseInsightReport.ts
@@ -29,9 +29,9 @@ const getCourseInsightReport = async (request: EdgeFunctionRequest) => {
     const numCompleted = enrollments.filter((enrollment) => enrollment.status === CourseStatus.COMPLETED).length;
 
     const completedAttempts = courseAttempts.filter((attempt) => attempt.pass == true);
-    const averageTime = completedAttempts.reduce((sum, attempt) => {
+    const averageTime = completedAttempts.length ? (completedAttempts.reduce((sum, attempt) => {
         return sum + (new Date(attempt.end_time) - new Date(attempt.start_time));
-    }, 0) / completedAttempts.length / 1000; // Time in seconds
+    }, 0) / completedAttempts.length / 1000) : null; // Time in seconds
 
     request.log(`Course has ${numEnrolled} enrollments, ${numStarted} started users, ${numCompleted} completions, and an average completion time of ${averageTime} seconds`);
 
