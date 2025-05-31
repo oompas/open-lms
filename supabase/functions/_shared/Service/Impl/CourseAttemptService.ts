42b47468ea7f305374861f1649dd49f11af6cc18
Reid Moffat <18rem8@queensu.ca>
2025-05-31 13:24:50 -0400

    clean

--- supabase/functions/_shared/Service/Impl/CourseAttemptService.ts
+++ supabase/functions/_shared/Service/Impl/CourseAttemptService.ts
@@ -7,12 +7,6 @@ class _courseAttemptService extends IService {
 
     TABLE_NAME = "course_attempt";
 
-    public async completeAttempt(id: number, pass: boolean) {
-        // TODO: throw error if there is one here (make a general update/add/etc in parent class that handles
-        //  errors, do this for everything!)
-        await adminClient.from(this.TABLE_NAME).update({ pass, end_time: getCurrentTimestampTz() }).eq('id', id);
-    }
-
     /**
      * Starts a new course attempt for a given course and user
      */
